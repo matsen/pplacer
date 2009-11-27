102f93b49c3b596a744a6a21dfbfa952314c83bc
Erick Matsen <ematsen@gmail.com>
2009-11-26 17:30:45 -0800

    last commit before putting in the similarity-based acceleration 	modified:   core.ml

--- core.ml
+++ core.ml
@@ -2,13 +2,6 @@
  * This file is part of pplacer. pplacer is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. pplacer is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with pplacer. If not, see <http://www.gnu.org/licenses/>.
  *
  * here we actually do the work.
- *
- * idea: implement profile-dag, which records the order of locations in the
- * tree, and finds the one which best matches the current query sequence.
- * D of IntMap | T of pquery
- * insertion
- * if we do maps, need to replace mapi with a recursive fun which passes along
- * the data structure
 *)
 
 open Fam_batteries
