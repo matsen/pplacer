f88c7c44dfafaf597f174c43a3af65184316f50f
Erick Matsen <ematsen@gmail.com>
2010-03-02 04:03:53 -0800

    better comments for minimization

--- minimization.ml
+++ minimization.ml
@@ -14,13 +14,16 @@ let isOKStart f start left right =
   let starty = f start in
   starty < f left || starty < f right
 
+(* run the GSL Brent algorithm. the work below is to find an appropriate
+ * starting point, i.e. a location x such that f(x) < min(f(left), f(right)).
+ *)
 let brent f raw_start left right tolerance = 
   if left >= raw_start || raw_start >= right then
     failwith 
     (Printf.sprintf 
       "Minimization.brent: start values don't satisfy %g < %g < %g" 
       left raw_start right);
-  (* find a starting point via bisection *)
+  (* find a starting point via bisection. *)
   let lefty = f left
   and righty = f right in
   let smaller = if lefty < righty then left else right in
@@ -28,13 +31,18 @@ let brent f raw_start left right tolerance =
   (* find the start *)
   let rec find_start prevStart iterNum = 
     let prevVal = f prevStart in
-    if prevVal < miny then prevStart
+    if prevVal < miny then 
+      (* we have found an appropriate starting point *)
+      prevStart 
     else if abs_float (prevStart -. smaller) < tolerance then
+      (* we are already within the specified tolerance via bisection *)
       if prevVal < miny then raise (FoundMin prevStart)
       else raise (FoundMin smaller)
     else if iterNum > maxIter then 
       failwith "Minimization.brent: couldn't find start!"
-    else find_start ((prevStart +. smaller) /. 2.) (iterNum+1)
+    else 
+      (* bisect *)
+      find_start ((prevStart +. smaller) /. 2.) (iterNum+1)
   in
   (* actually do the iteration *)
   try 
@@ -42,16 +50,14 @@ let brent f raw_start left right tolerance =
         Gsl_min.make Gsl_min.BRENT f (find_start raw_start 1) left right in
     let rec run whichStep = 
       if whichStep > maxIter then raise ExceededMaxIter
-      else (
+      else begin
         Gsl_min.iterate iterator;
         let interLow, interHigh = Gsl_min.interval iterator in
-        (* Printf.printf "%f\t%f\t%f\n" (Gsl_min.minimum iterator) interLow
-         * interHigh;*)
         if interHigh -. interLow < tolerance then 
           Gsl_min.minimum iterator
         else
           run (whichStep+1)
-      )
+      end
     in
     run 1
   with
