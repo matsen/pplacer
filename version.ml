dd0fcb3e733eb62a33e29fa5c3feb38b104292d6
Erick Matsen <ematsen@gmail.com>
2010-03-16 20:22:38 -0700

    modified:   version.ml

--- version.ml
+++ version.ml
@@ -4,7 +4,7 @@
 
 let chop_revision s = Str.replace_first (Str.regexp "\\.r[0-9]+") "" s
 
-let version_revision = "v1.0.r006"
+let version_revision = "v1.0.r007"
 let version = chop_revision version_revision
 
 
