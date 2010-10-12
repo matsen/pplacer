9059d37748b4bb01374172536d7db7c38fe1aa34
Erick Matsen <ematsen@gmail.com>
2010-10-12 07:15:13 -0700

    new file:   full_splits.ml

--- mokaphy_src/cmds.ml
+++ mokaphy_src/cmds.ml
@@ -136,6 +136,7 @@ let pd prefs prl =
 (* *** PDFRAC PDFRAC PDFRAC PDFRAC PDFRAC *** *)
 
 let x = Pdfrac.hbflim_of_inda
+let x = Full_splits.make_proximal_map
 
 let pdfrac prefs prl = 
   let ch = ch_of_fname (Mokaphy_prefs.PDFrac.out_fname prefs) in
