00068354d66b18ba01e262105f51b95572147371
Erick Matsen <ematsen@gmail.com>
2010-10-19 00:45:20 -0700

    fixed complete_distr.sh

--- complete_distr.sh
+++ complete_distr.sh
@@ -5,12 +5,7 @@ OS=`uname -s`
 DIRNAME=pplacer_v1.0_$OS.$KERNEL
 
 rm -f $DIRNAME.tar.gz
-mkdir $DIRNAME
-
-for i in `find _build -regex .*native | sed 's/_build\///; s/.native//'`
-do 
-  cp _build/$i.native $DIRNAME/$i; 
-done
+cp -r bin $DIRNAME
 
 tar -cf $DIRNAME.tar $DIRNAME
 gzip $DIRNAME.tar
