ceffef574a18d03491a4c6fd4f5bb8dad2efd511
Erick Matsen <ematsen@gmail.com>
2010-10-28 04:06:39 -0700

    removed old material from kr_core.ml

--- mokaphy_src/kr_core.ml
+++ mokaphy_src/kr_core.ml
@@ -76,217 +76,59 @@ let list_get_same_tree = function
  * run pair_core for each unique pair 
  *)
 let core ch prefs prl = 
-  let n_samples = Mokaphy_prefs.KR.n_samples prefs in
-  let my_pre_of_pr = 
-    Mass_map.Pre.of_placerun
-      (Mokaphy_prefs.weighting_of_bool (Mokaphy_prefs.KR.weighted prefs))
-      (Mokaphy_prefs.criterion_of_bool (Mokaphy_prefs.KR.use_pp prefs))
+  if List.length prl <= 1 then 
+    invalid_arg "can't do KR with fewer than two place files";
+  let n_samples = Mokaphy_prefs.KR.n_samples prefs 
+  and is_weighted = Mokaphy_prefs.KR.weighted prefs
+  and use_pp = Mokaphy_prefs.KR.use_pp prefs
   in
-  let pra = Array.of_list prl in
+  let my_pre_of_pr = Cmds_common.pre_of_pr ~is_weighted ~use_pp
+  and pra = Array.of_list prl in
   let prea = Array.map my_pre_of_pr pra in
-  if Array.length prea <= 1 then 
-    print_endline "can't do KR with fewer than two place files"
+  let context pr1 pr2 = 
+    Printf.sprintf "comparing %s with %s" 
+      (Placerun.get_name pr1) (Placerun.get_name pr2)
+  and p = Mokaphy_prefs.KR.p_exp prefs
+  and t = list_get_same_tree prl
+  in
+  let u = 
+    Uptri.init
+      (Array.length prea)
+      (fun i j ->
+        wrapped_pair_core
+          (context pra.(i) pra.(j)) p n_samples t prea.(i) prea.(j))
+  in
+  (* matrix funniness *)
+  let names = Array.map Placerun.get_name pra
+    (* if Mokaphy_prefs.KR.matrix prefs then 2. else  *)
+  and print_pvalues = (* Mokaphy_prefs.KR.matrix prefs || *)
+                      n_samples > 0
+  in
+  if Mokaphy_prefs.KR.list_output prefs then begin
+    String_matrix.write_padded ch
+      (Array.append
+        [|Array.append
+          [|"sample_1"; "sample_2"; Printf.sprintf "Z_%g" p;|]
+          (if print_pvalues then [|"p_values"|] else [||])|]
+        (let m = ref [] in
+        Uptri.iterij
+          (fun i j r -> 
+            m := 
+              (Array.of_list
+                ([names.(i); names.(j); string_of_float r.distance] @
+                (if print_pvalues then [string_of_float (get_p_value r)]
+                else [])))::!m)
+          u;
+        Array.of_list (List.rev !m)))
+  end
   else begin
-    let context pr1 pr2 = 
-      Printf.sprintf "comparing %s with %s" 
-        (Placerun.get_name pr1) (Placerun.get_name pr2)
-    and p = Mokaphy_prefs.KR.p_exp prefs
-    and t = list_get_same_tree prl
-    in
-    let u = 
-      Uptri.init
-        (Array.length prea)
-        (fun i j ->
-          wrapped_pair_core
-            (context pra.(i) pra.(j)) p n_samples t prea.(i) prea.(j))
-    in
-    (* matrix funniness *)
-    let names = Array.map Placerun.get_name pra
-      (* if Mokaphy_prefs.KR.matrix prefs then 2. else  *)
-    and print_pvalues = (* Mokaphy_prefs.KR.matrix prefs || *)
-                        n_samples > 0
-    in
-    if Mokaphy_prefs.KR.list_output prefs then begin
-      String_matrix.write_padded ch
-        (Array.append
-          [|Array.append
-            [|"sample_1"; "sample_2"; Printf.sprintf "Z_%g" p;|]
-            (if print_pvalues then [|"p_values"|] else [||])|]
-          (let m = ref [] in
-          Uptri.iterij
-            (fun i j r -> 
-              m := 
-                (Array.of_list
-                  ([names.(i); names.(j); string_of_float r.distance] @
-                  (if print_pvalues then [string_of_float (get_p_value r)]
-                  else [])))::!m)
-            u;
-          Array.of_list (List.rev !m)))
+    Printf.fprintf ch "Z_%g distances:\n" p;
+    Mokaphy_base.write_named_float_uptri ch names (Uptri.map get_distance u);
+    if Mokaphy_prefs.KR.matrix prefs || Mokaphy_prefs.KR.n_samples prefs > 0 then begin
+      Printf.fprintf ch "Z_%g p-values:\n" p;
+      Mokaphy_base.write_named_float_uptri ch names (Uptri.map get_p_value u);
     end
-    else begin
-      Printf.fprintf ch "Z_%g distances:\n" p;
-      Mokaphy_base.write_named_float_uptri ch names (Uptri.map get_distance u);
-      if Mokaphy_prefs.KR.matrix prefs || Mokaphy_prefs.KR.n_samples prefs > 0 then begin
-        Printf.fprintf ch "Z_%g p-values:\n" p;
-        Mokaphy_base.write_named_float_uptri ch names (Uptri.map get_p_value u);
-      end
-    end;
   end;
   ()
 
 
-
- (* DEPRECATED *)
- (* DEPRECATED *)
- (* DEPRECATED *)
- (* DEPRECATED *)
-
-(*
-
-(* makes an array of shuffled placeruns (identity of being in first or second
- * one shuffled randomly, but number in each the same) 
- * *)
-let make_shuffled_prs n_shuffles pr1 pr2 = 
-  let pq1 = Placerun.get_pqueries pr1
-  and pq2 = Placerun.get_pqueries pr2
-  in
-  let pquery_arr = Array.of_list (pq1 @ pq2)
-  and n1 = List.length pq1
-  and n2 = List.length pq2
-  in
-  let pquery_sub start len = 
-    Array.to_list (Array.sub pquery_arr start len)
-  in
-  let make_pr pr num pqueries = 
-    Placerun.make
-      (Placerun.get_ref_tree pr)
-      (Placerun.get_prefs pr)
-      ((Placerun.get_name pr)^"_shuffle_"^(string_of_int num))
-      pqueries
-  in
-  ListFuns.init 
-    n_shuffles
-    (fun num ->
-      Mokaphy_base.shuffle pquery_arr;
-      (make_pr pr1 num (pquery_sub 0 n1),
-      make_pr pr2 num (pquery_sub n1 n2)))
-
-let weighting_of_prefs prefs =
-  if Mokaphy_prefs.KR.weighted prefs then Mass_map.Weighted 
-  else Mass_map.Unweighted 
-
-(* this is now disabled *)
-let old_pair_core prefs criterion pr1 pr2 =
-  (*
-  let rng = Gsl_rng.make Gsl_rng.KNUTHRAN2002 in
-  Gsl_rng.set rng (Nativeint.of_int (Mokaphy_prefs.KR.seed prefs));
-  *)
-  let p = (Mokaphy_prefs.KR.p_exp prefs) in
-  let weighting = weighting_of_prefs prefs in
-  if Mokaphy_prefs.KR.ddensity prefs then
-    R_plots.write_ddensity pr1 pr2;
-  (*
-   * disabled matrix
-  if Mokaphy_prefs.KR.matrix prefs then begin
-    let (distance, p_value) =
-      Matrix_sig.dist_and_p weighting criterion rng pr1 pr2 in
-    {distance = distance; p_value = Some p_value}
-  end
-  else 
-    if Mokaphy_prefs.KR.normal prefs then begin
-      (* normal approx mode *)
-      if 0 >= Mokaphy_prefs.KR.n_samples prefs then
-        failwith "Please ask for some number of normal samples greater than zero. If you want to disable sampling do not use the --normal option";
-      let resampled_dists = 
-        Normal_approx.normal_pair_approx rng weighting
-        criterion (Mokaphy_prefs.KR.n_samples prefs) p pr1 pr2
-      in
-      (* here we shadow original_dist with one we know is unweighted *)
-      let original_dist = 
-        Kr_distance.pair_distance
-        weighting
-        criterion 
-        p 
-        pr1 
-        pr2
-    in
-    R_plots.write_density 
-      "normal"
-      (Placerun.get_name pr1)
-      (Placerun.get_name pr2)
-      original_dist 
-      resampled_dists
-      p;
-    { distance = original_dist;
-      p_value = 
-        Some 
-          (Mokaphy_base.list_onesided_pvalue 
-            resampled_dists 
-            original_dist)}
-  end
-  else 
-    *)
-    begin
-    let calc_dist = 
-      Kr_distance.pair_distance
-        weighting 
-        criterion 
-        p in
-    let original_dist = calc_dist pr1 pr2 in
-    (* shuffle mode *)
-    let p_value = 
-      if 0 < Mokaphy_prefs.KR.n_samples prefs then begin
-        let shuffled_list = 
-          make_shuffled_prs (Mokaphy_prefs.KR.n_samples prefs) pr1 pr2 in
-        let shuffled_dists = 
-          List.map 
-            (fun (spr1,spr2) -> calc_dist spr1 spr2)
-            shuffled_list
-        in
-        if Mokaphy_prefs.KR.density prefs then
-          R_plots.write_density 
-            "shuffle_density"
-            (Placerun.get_name pr1)
-            (Placerun.get_name pr2)
-            original_dist 
-            shuffled_dists 
-            p;
-        if Mokaphy_prefs.KR.p_plot prefs then
-          R_plots.write_p_plot weighting criterion pr1 pr2;
-        if Mokaphy_prefs.KR.box_plot prefs then
-          R_plots.write_boxplot weighting criterion pr1 pr2 shuffled_list;
-        (* make a barycenter plot of significance *)
-        if Mokaphy_prefs.KR.bary_density prefs then begin
-          let bary_dist xpr1 xpr2 = 
-            Barycenter.calc_dist
-              (weighting_of_prefs prefs)
-              criterion 
-              xpr1
-              xpr2
-          in
-          let original_bdist = bary_dist pr1 pr2
-          and shuffled_bdists = 
-            List.map 
-              (fun (spr1,spr2) -> calc_dist spr1 spr2)
-              shuffled_list
-          in
-          R_plots.write_density 
-            "bary_density"
-            (Placerun.get_name pr1)
-            (Placerun.get_name pr2)
-            original_bdist 
-            shuffled_bdists 
-            p;
-        end;
-        (* return the KR p-value *)
-        Some
-          (Mokaphy_base.list_onesided_pvalue 
-            shuffled_dists 
-            original_dist)
-      end
-      else None
-    in
-    {distance = original_dist; p_value = p_value}
-  end
-
-    *)
