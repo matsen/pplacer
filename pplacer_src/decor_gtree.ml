30f6a31bf584222a507251074cb391f8637fb322
Erick Matsen <ematsen@gmail.com>
2010-11-08 20:13:08 -0800

    Decor_gtree.map_add_decor_listly refactoring

--- pplacer_src/decor_gtree.ml
+++ pplacer_src/decor_gtree.ml
@@ -14,18 +14,16 @@ let compare t1 t2 = Gtree.compare Decor_bark.compare t1 t2
 let of_newick_gtree t = Gtree.map_bark_map Decor_bark.of_newick_bark t
 let to_newick_gtree t = Gtree.map_bark_map Decor_bark.to_newick_bark t
 
+(* append (id, decorl) to the bark map *)
+let map_add_decor_listly id decorl barkm = 
+  IntMap.add 
+    id
+    ((if IntMap.mem id barkm then IntMap.find id barkm 
+    else new Decor_bark.decor_bark `Empty)
+      #append_decor decorl)
+    barkm
+
 (* decor_map is an IntMap to a Decor.decoration list *)
 let add_decor_by_map t decor_map = 
-  let bark_map = Gtree.get_bark_map t in
   Gtree.set_bark_map t
-    (IntMap.fold
-      (fun id decor_list ->
-        IntMap.add 
-          id
-          ((if IntMap.mem id bark_map then IntMap.find id bark_map 
-          else new Decor_bark.decor_bark `Empty)
-            #append_decor decor_list))
-      decor_map
-      bark_map)
-
-
+    (IntMap.fold map_add_decor_listly decor_map (Gtree.get_bark_map t))
