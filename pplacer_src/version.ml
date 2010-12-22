1e8c1a34cf13dd22a13ec64f1d175715e77fd80f
Erick Matsen <matsen@fhcrc.org>
2010-12-22 11:48:11 -0800

    v1.0.r031

--- pplacer_src/version.ml
+++ pplacer_src/version.ml
@@ -4,7 +4,7 @@
 
 let chop_revision s = Str.replace_first (Str.regexp "\\.r[0-9]+") "" s
 
-let version_revision = "v1.0.r030"
+let version_revision = "v1.0.r031"
 let version = chop_revision version_revision
 
 
