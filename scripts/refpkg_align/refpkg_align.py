c24c043da3bb8dcff2f7eb935cf84a4c6f1b61f2
Brian Hodges <bhodges@fhcrc.org>
2010-12-22 10:53:28 -0800

    work on hmmer alignment and splitting of alignment into two seperate files with ref seqs and fragments

--- scripts/refpkg_align/refpkg_align.py
+++ scripts/refpkg_align/refpkg_align.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import sys, os, string, argparse, re
+import sys, os, string, argparse, re, subprocess
 from string import Template
 from Bio import SeqIO, AlignIO
 from Bio.Seq import Seq, SeqRecord
@@ -21,43 +21,84 @@ def main():
 
 def hmmer_align(reference_package, sequence_files, out_prefix):
     """
+    Create an alignment with hmmer. Then, separate out reference sequences 
+    from the fragments into two separate files.
     """
   
     # Determine the name of the reference package, excluding any other 
     # elements in its path.
     reference_package_name = list(os.path.split(reference_package)).pop()
+    reference_package_name_prefix = os.path.splitext(reference_package_name)[0]
 
     # Set default prefix if unspecified.
+    out_prefix_arg = out_prefix
     if out_prefix is None:
-        out_prefix = reference_package + '.' # Sequence file name will be appended within for loop.
+        out_prefix = os.path.join(reference_package, reference_package_name_prefix) + '.' # Sequence file name will be appended within for loop.
         
         
     
     # hmmalign must be in PATH for this to work.
 
     command_regex = re.compile(r'\s+')
-    hmmer_template = Template('hmmalign --outformat afa -o $temp_file' + '.afa --mapali ' + \
-                              '$reference_package/$reference_package_name' + \
-                              '.sth $reference_package/$reference_package_name' + \
-                              '.hmm $sequence_file')
+    hmmer_template = Template('hmmalign --outformat afa -o $tmp_file' + ' --mapali ' + \
+                              '$reference_package/$reference_package_name_prefix' + \
+                              '.ref.sth $reference_package/$reference_package_name_prefix' + \
+                              '.ref.hmm $sequence_file')
     for sequence_file in sequence_files:
+        
         # Determine a name for the temporary output file.
-        temp_file = sequence_file + '.' + str(os.getppid())
+        tmp_file = sequence_file + '.' + str(os.getppid()) + '.afa'
+        sequence_file_name = list(os.path.split(sequence_file)).pop()
+        sequence_file_name_prefix = os.path.splitext(sequence_file_name)[0]
 
         hmmalign_command = hmmer_template.substitute(reference_package=reference_package, 
                                                      sequence_file=sequence_file,
-                                                     reference_package_name=reference_package_name,
-                                                     temp_file=temp_file,
+                                                     reference_package_name_prefix=reference_package_name_prefix,
+                                                     tmp_file=tmp_file,
                                                     )
+        try:
+            child = subprocess.Popen(hmmalign_command,
+                                     stdin=None,
+                                     stdout=None,
+                                     stderr=None,
+                                     shell=(sys.platform!="win32"))
+            return_code = child.wait()
+        
+            # If return code was not 1, split off the reference sequences from the fragments.
+            if not return_code:
+                # Determine output file names.  Set to default if --outprefix was not specified.
+                out_refs, out_frags = [os.path.join(reference_package, out_prefix + '.ref.fasta'), 
+                                       os.path.join(reference_package, out_prefix + '.frag.fasta')]
+                if not out_prefix_arg:
+                    out_refs = out_prefix + sequence_file_name_prefix + ".ref.fasta"
+                    out_frags = out_prefix + sequence_file_name_prefix + ".frag.fasta"
+ 
+                frag_names = names(SeqIO.parse(sequence_file, "fasta"))
+                in_seqs = SeqIO.parse(tmp_file, "fasta")
+                SeqIO.write(id_filter(in_seqs, lambda(idstr): idstr not in frag_names), out_refs, "fasta")
+                in_seqs = SeqIO.parse(tmp_file, "fasta")
+                SeqIO.write(id_filter(in_seqs, lambda(idstr): idstr in frag_names), out_frags, "fasta")
+        except:
+            raise
+        finally:
+            os.remove(tmp_file)
 
-        print hmmalign_command
-                                                     
-   #     child = subprocess.Popen(hmmalign_command,
-   #                              stdin=None,
-   #                              stdout=None,
-   #                              stderr=None,
-   #                              shell=(sys.platform!="win32"))
-   #     return_code = child.wait()
+
+def names(records):
+    """
+    Get the sequence names.
+    """
+    s = set()
+    for record in records:
+        s.add(record.id)
+    return(s)
+
+
+def id_filter(in_seqs, f):
+    for record in in_seqs:
+        if (f(record.id)):
+            yield record
+
     
 
 def parse_arguments():
@@ -87,64 +128,6 @@ def reference_package(reference_package):
 
 
     
-
-
-##refpkg["aln_fasta"]
-
-# i would like to specify a refpkg and a set of sequences to align as positional arguments like
-# refpkg_align COG0003.refpkg to_align.fasta
-# an -o option provides an out prefix, which defaults to a concatenation of the refpkg name and the name of the sequences to align, so in this case
-##out_prefix = "COG0003.to_align"
-
-# say we have slurped the corresponding CONTENTS.json file into a dictionary called refpkg.
-
-# step zero: find a temporary filename that we can write to safely. here we just use something dumb.
-##aligned = "tmp.afa"
-
-# step one: run hmmalign, which can be found in /mnt/silo/silo_researcher/Matsen_F/MatsenGrp/local/bin
-# here we would run it like
-#hmmalign --outformat afa -o tmp.afa --mapali COG0003.refpkg/COG0003.ref.sth COG0003.refpkg/COG0003.ref.hmm to_align.fasta 
-# which in terms of the dictionary would be
-#hmmalign --outformat afa -o tmp.afa --mapali refpkg["aln_sto"] refpkg["profile"] to_align.fasta 
-
-# step two: split off the reference sequences from the fragments.
-# hmmalign puts them together, which is good that they then appear in the same alignment frame, but bad in that we want them pulled into separate files
-# here are the file names for the out files:
-##out_refs = out_prefix+".ref.fasta"
-##out_frags = out_prefix+".frag.fasta"
-
-# here is a function for getting a set of ids
-##def names(records):
-##    """"get the sequence names."""
-##    s = set()
-##    for record in records:
-##        s.add(record.id)
-##    return(s)
-
-##frag_names = names(SeqIO.parse("to_align.fasta", "fasta"))
-
-# here is where we filter the seqs
-##def id_filter(f):
-##    for record in in_seqs:
-##	if (f(record.id)):
-##	    yield record
-
-# this is not terribly elegant, in that we start from the beginning two times
-# but i don't really mind.
-
-# if we were using file objects rather than generator functions we could have 
-# if (f(record.id)):
-#     <write in out_frags>
-# else:
-#     <write in out_refs>
-
-##in_seqs = SeqIO.parse(aligned, "fasta")
-##SeqIO.write(id_filter(lambda(idstr): idstr not in frag_names), out_refs, "fasta")
-##in_seqs = SeqIO.parse(aligned, "fasta")
-##SeqIO.write(id_filter(lambda(idstr): idstr in frag_names), out_frags, "fasta")
-
-
-
 
 if __name__ == '__main__':
     sys.exit(main())
