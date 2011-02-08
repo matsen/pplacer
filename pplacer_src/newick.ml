16b952f8bd4f82e3960c9a283e837b84b4207598
Aaron Gallagher <habnabit@gmail.com>
2011-02-08 15:38:09 -0800

    Now also writing out Newick format with edge labels.
     * Newick.to_string (and Newick.string_of_bark) take a named parameter
       ~with_edge_labels which toggles whether they'll be written out.
     * Added unit tests for the parsing and stringification.
     * Added Test_util.gtree_equal to support the tests.

--- pplacer_src/newick.ml
+++ pplacer_src/newick.ml
@@ -13,7 +13,7 @@ type t = Newick_bark.newick_bark Gtree.gtree
 
 let compare t1 t2 = Gtree.compare Newick_bark.compare t1 t2
 
-let to_numbered t = 
+let to_numbered t =
   Gtree.mapi_bark_map (fun i x -> x#to_numbered i) t
 
 let make_boot_id t =
@@ -21,36 +21,39 @@ let make_boot_id t =
 
 (* output *)
 
-let string_of_bark t id = 
-    match Gtree.get_bark_opt t id with
+let string_of_bark ?(with_edge_labels = false) t id =
+  let base = match Gtree.get_bark_opt t id with
     | Some b -> b#to_newick_string
     | None -> ""
+  in if with_edge_labels
+    then Printf.sprintf "%s[%d]" base id
+    else base
 
-let to_string_gen f t = 
+let to_string_gen f t =
     (Gtree.recur
       (fun id below ->
         "("^(String.concat "," below)^")"^(f t id))
       (f t)
       t)^";"
 
-let to_string t = to_string_gen string_of_bark t
+let to_string ?(with_edge_labels = false) t = to_string_gen (string_of_bark ~with_edge_labels) t
 
 let write ch t = Printf.fprintf ch "%s\n" (to_string t)
 
-let tree_list_to_file trees fname = 
+let tree_list_to_file trees fname =
   let ch = open_out fname in
   List.iter (write ch) trees;
   close_out ch
 
 let to_file t fname = tree_list_to_file [t] fname
 
-let ppr ff t = 
-  let ppr_bark ff id = 
-    Format.pp_print_string ff (string_of_bark t id) in 
+let ppr ff t =
+  let ppr_bark ff id =
+    Format.pp_print_string ff (string_of_bark t id) in
   let rec aux ff = function
     | Stree.Node(id, tL) ->
       Format.fprintf ff "@[(%a)%a@]"
-        (Ppr.ppr_gen_list_inners "," aux) tL 
+        (Ppr.ppr_gen_list_inners "," aux) tL
         ppr_bark id
     | Stree.Leaf(id) ->
         ppr_bark ff id
@@ -62,37 +65,37 @@ let ppr ff t =
 (* input *)
 
 (* count the number of occurrences of char c in str *)
-let count_n_occurrences c str = 
+let count_n_occurrences c str =
   let count = ref 0 in
   String.iter (fun d -> if c = d then incr count) str;
   !count
 
-let check_string s = 
-  let n_open = count_n_occurrences '(' s 
+let check_string s =
+  let n_open = count_n_occurrences '(' s
   and n_closed = count_n_occurrences ')' s in
   if n_open <> n_closed then
     Printf.printf "warning: %d open parens and %d closed parens\n" n_open n_closed;
   ()
 
-let of_lexbuf lexbuf = 
+let of_lexbuf lexbuf =
   try
     Newick_parser.tree Newick_lexer.token lexbuf
-  with 
+  with
   | Parsing.Parse_error -> failwith "couldn't parse tree!"
 
-let of_string s = 
+let of_string s =
   check_string s;
   try
-    of_lexbuf 
+    of_lexbuf
     (Lexing.from_string (Str.replace_first (Str.regexp ");") "):0.;" s))
   with
   | Failure s -> failwith("problem parsing tree: "^s)
 
-let of_file fname = 
+let of_file fname =
   match
-    List.filter 
-      (fun line -> 
-        not (Str.string_match (Str.regexp "^[ \t]*$") line 0)) 
+    List.filter
+      (fun line ->
+        not (Str.string_match (Str.regexp "^[ \t]*$") line 0))
       (File_parsing.string_list_of_file fname)
   with
     | [] -> failwith ("empty file in "^fname)
---
+++ tests/pplacer/test_edge_labels.ml
@@ -0,0 +1,23 @@
+open OUnit
+open Test_util
+
+let unlabeled = "((A:2,B:9):7,C:5,D:1):0;"
+let labeled = "((A:2[0],B:9[1]):7[2],C:5[3],D:1[4]):0[5];"
+let higher_labeled = "((A:2[20],B:9[21]):7[22],C:5[23],D:1[24]):0[25];"
+
+let test_parsed_equality _ =
+  let p1, p2 = Newick.of_string unlabeled, Newick.of_string labeled in
+  "labeled != unlabled" @? gtree_equal p1 p2
+
+let test_string_representation _ =
+  let p1 = Newick.of_string higher_labeled in
+  let s1 = Newick.to_string ~with_edge_labels:true p1 in
+  let p2 = Newick.of_string s1 in
+  let s2 = Newick.to_string ~with_edge_labels:true p2 in
+  "first-parsed != second-parsed" @? gtree_equal p1 p2;
+  "first-string != second-string" @? (s1 = s2)
+
+let suite = [
+  "parsed_equality" >:: test_parsed_equality;
+  "string_representation" >:: test_string_representation;
+]
