c877be0bde98f64f9569bbbceac17274ef6d993d
Aaron Gallagher <habnabit@gmail.com>
2011-02-14 18:46:38 -0800

    Fixing some JSON stuff.
     * The serializer wasn't quoting strings; I just added that.
     * Apparently the unquoting stuff wasn't working either and I just hadn't tested for it. That's fixed too now!

--- json_src/json.ml
+++ json_src/json.ml
@@ -4,13 +4,27 @@ let of_string s =
   let lexbuf = Lexing.from_string s in
   Jsonparse.parse Jsonlex.token lexbuf
 
+let to_escape = Str.regexp "\\([\\\\\"/\b\012\n\r\t]\\)"
+let quote = Str.global_substitute to_escape begin fun s ->
+  match Str.replace_matched "\\1" s with
+    | "\\" -> "\\\\"
+    | "\"" -> "\\\""
+    | "/" -> "\\/"
+    | "\b" -> "\\b"
+    | "\012" -> "\\f"
+    | "\n" -> "\\n"
+    | "\r" -> "\\r"
+    | "\t" -> "\\t"
+    | s -> failwith ("invalid " ^ s)
+end
+
 let rec to_formatter ff o =
   let pf = Format.printf in
   let _ = match o with
     | Bool b -> pf "%s" (string_of_bool b)
     | Int i -> pf "%d" i
     | Float f -> pf "%f" f
-    | String s -> pf "\"%s\"" s
+    | String s -> pf "\"%s\"" (quote s)
     | Object o ->
       pf "{@[@,";
       let _ = Hashtbl.fold (fun k v is_first ->
