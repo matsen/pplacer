ea7c321ec2e4f7e891334c4aa28853ad4e4a21a2
Aaron Gallagher <habnabit@gmail.com>
2011-02-17 11:51:19 -0800

    Adding the XML namespaces to phyloxml output.

--- pplacer_src/phyloxml.ml
+++ pplacer_src/phyloxml.ml
@@ -61,7 +61,7 @@ let load fname =
   }
 
 let xml_of_pxdata pxd =
-  Xml.Element("phyloxml", pxd.data_attribs, List.map xml_of_pxtree pxd.trees)
+  Xml.Element("phyloxml", phyloxml_attrs @ pxd.data_attribs, List.map xml_of_pxtree pxd.trees)
 
 let pxdata_to_file fname pxd =
   let ch = open_out fname in
