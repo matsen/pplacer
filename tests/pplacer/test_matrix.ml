1bd579a8b778e3b7c46acdfe9972f427c229198d
Erick Matsen <matsen@fhcrc.org>
2011-02-23 19:49:23 -0800

    added dediag test

--- tests/pplacer/test_matrix.ml
+++ tests/pplacer/test_matrix.ml
@@ -4,12 +4,12 @@ open Fam_gsl_matvec
 
 let mm = allocMatMatMul;;
 
-(* this checks to make sure that symmEigs is giving us the eigenvectors as
+(* this checks to make sure that symmv is giving us the eigenvectors as
  * column vectors. *)
 let check_symmv m =
   let (l,v) = Gsl_eigen.symmv (`M(m)) in
   let m' = mm v (mm (diagOfVec l) (alloc_transpose v)) in
-  "symmEigs not giving column vectors " @? matrices_approximately_equal m m'
+  "symmEigs not giving column vectors" @? matrices_approximately_equal m m'
 
 let test_symmv _ =
   let b = Gsl_matrix.of_arrays [|[|-. 1.; 0.15|];[|0.15; -.2.|]|] in
