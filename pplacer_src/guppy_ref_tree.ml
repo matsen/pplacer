649ee0ebb08efa5ed53102305a26f4d355283018
Erick Matsen <matsen@fhcrc.org>
2011-02-27 20:11:24 -0800

    ref trees are now specified with no prefix-- better design

--- pplacer_src/guppy_ref_tree.ml
+++ pplacer_src/guppy_ref_tree.ml
@@ -1,6 +1,17 @@
 open Guppy_cmdobjs
 open Subcommand
 
+let trees_of_refpkg path =
+  let prefix = Mokaphy_common.chop_suffix_if_present path ".refpkg" in
+  match Refpkg.refpkgo_of_path path with
+  | None -> []
+  | Some rp ->
+      let (taxt, _) = Tax_gtree.of_refpkg_unit rp in
+      [
+        Some (prefix^".ref"), Refpkg.get_tax_ref_tree rp;
+        Some (prefix^".tax"), taxt;
+      ]
+
 class cmd () =
 object
   inherit subcommand () as super
@@ -12,20 +23,10 @@ object
   method desc =
     "generates a taxonomically annotated reference tree and an \
     induced taxonomic tree in phyloXML format"
-  method usage = "usage: ref_tree -c my.refpkg -o my.xml"
+  method usage = "usage: ref_tree -o my.xml my1.refpkg [my2.refpkg ...]"
 
-  method action _ =
-    let ch = super_outfile#out_channel
-    and prefix = Mokaphy_common.chop_suffix_if_present (fv refpkg_path) ".refpkg"
-    in
-    match Refpkg.refpkgo_of_path (fv refpkg_path) with
-    | None -> failwith "please supply a reference package name with -c."
-    | Some rp ->
-    let (taxt, _) = Tax_gtree.of_refpkg_unit rp in
-    Phyloxml.pxdata_to_channel ch
+  method action pathl =
+    Phyloxml.pxdata_to_channel super_outfile#out_channel
       (Phyloxml.pxdata_of_named_gtrees
-        [
-          Some (prefix^".ref"), Refpkg.get_tax_ref_tree rp;
-          Some (prefix^".tax"), taxt;
-        ])
+        (List.flatten (List.map trees_of_refpkg pathl)))
 end
