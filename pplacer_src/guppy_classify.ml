407b3fb4bb1af81fbeb421f80c3ea58e288a3865
Aaron Gallagher <habnabit@gmail.com>
2011-03-01 12:15:22 -0800

    Fixing up `guppy classify`.
     * Did some refactoring to allow for multiple output formats.
     * Added CSV output.

--- pplacer_src/guppy_classify.ml
+++ pplacer_src/guppy_classify.ml
@@ -36,42 +36,36 @@ let classif_stral td name desired_rank m =
       |])
     (Tax_id.TaxIdMapFuns.to_pairs m)
 
-let classify how criterion rp prl =
-  let td = Refpkg.get_taxonomy rp in
-  let n_ranks = Tax_taxonomy.get_n_ranks td in
-  List.iter
-    (fun pr ->
-      let ch = open_out ((Placerun.get_name pr)^".class.tab") in
-      try
-        List.iter
-          (fun pq ->
-            let outl = ref [] in
-            let m = ref
-              (List.fold_right
-                (fun p ->
-                  TIAMR.add_by
-                    (how p)
-                    (criterion p))
-                (Pquery.place_list pq)
-                (TIAMR.M.empty))
-            in
-            for desired_rank=(n_ranks-1) downto 0 do
-              m := keymap_add_by (classify_at_rank td desired_rank) !m;
-              outl :=
-                (List.flatten
-                  (List.map
-                    (fun name -> classif_stral td name desired_rank !m)
-                    (Pquery.namel pq)))
-                @ (!outl);
-            done;
-            String_matrix.write_padded ch (Array.of_list (!outl));)
-          (Placerun.get_pqueries pr);
-          close_out ch;
-      with
-      | Placement.No_classif ->
-          invalid_arg
-            ((Placerun.get_name pr)^" contains unclassified queries!"))
-    prl
+let classify how criterion n_ranks td pr f =
+  try
+    List.iter
+      (fun pq ->
+        let outl = ref [] in
+        let m = ref
+          (List.fold_right
+             (fun p ->
+               TIAMR.add_by
+                 (how p)
+                 (criterion p))
+             (Pquery.place_list pq)
+             (TIAMR.M.empty))
+        in
+        for desired_rank=(n_ranks-1) downto 0 do
+          m := keymap_add_by (classify_at_rank td desired_rank) !m;
+          outl :=
+            (List.flatten
+               (List.map
+                  (fun name -> classif_stral td name desired_rank !m)
+                  (Pquery.namel pq)))
+          @ (!outl);
+        done;
+        f (!outl))
+      (Placerun.get_pqueries pr)
+  with
+    | Placement.No_classif ->
+      invalid_arg
+        ((Placerun.get_name pr)^" contains unclassified queries!")
+
 
 (* UI-related *)
 
@@ -83,20 +77,45 @@ object
 
   val use_pp = flag "--pp"
     (Plain (false, "Use posterior probability for our criteria."))
+  val csv_out = flag "--csv"
+    (Plain (false, "Write .class.csv files containing CSV data instead of a matrix."))
 
   method specl =
     super_refpkg#specl
-    @ [
-      toggle_flag use_pp;
-    ]
+  @ [
+    toggle_flag use_pp;
+    toggle_flag csv_out;
+  ]
 
   method desc =
-"outputs classification information in a tabular format"
+    "outputs classification information in a tabular format"
   method usage = "usage: classify [options] placefile[s]"
 
   method private placefile_action prl =
-    let rp = Refpkg.of_path (fv refpkg_path)
-    and criterion = if (fv use_pp) then Placement.post_prob else Placement.ml_ratio
+    let rp = Refpkg.of_path (fv refpkg_path) in
+    let criterion = if (fv use_pp) then Placement.post_prob else Placement.ml_ratio in
+    let td = Refpkg.get_taxonomy rp in
+    let n_ranks = Tax_taxonomy.get_n_ranks td in
+    let out_func pr =
+      if fv csv_out then
+        (fun outl ->
+          let prn = Placerun.get_name pr in
+          let ch = open_out (prn ^ ".class.csv") in
+          output_string ch "name,desired_rank,rank,tax_id,likelihood,origin\n";
+          List.iter
+            (fun arr -> Printf.fprintf ch "%s,%s\n" (String.concat "," (Array.to_list arr)) prn)
+            outl;
+          close_out ch)
+      else
+        (fun outl ->
+          let ch = open_out ((Placerun.get_name pr)^".class.tab") in
+          String_matrix.write_padded ch (Array.of_list outl);
+          close_out ch)
     in
-    classify Placement.contain_classif criterion rp prl
+    List.iter
+      (fun pr ->
+        classify Placement.contain_classif criterion n_ranks td pr (out_func pr))
+      prl
+
 end
+
