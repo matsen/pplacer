5e51efd92a5e632e92d1b55a81a2c113c3bb89c6
Aaron Gallagher <habnabit@gmail.com>
2011-03-02 15:13:28 -0800

    Fixed some code to use mat_approx_equal.

--- tests/pplacer/test_matrix.ml
+++ tests/pplacer/test_matrix.ml
@@ -9,7 +9,7 @@ let mm = allocMatMatMul;;
 let check_symmv m =
   let (l,v) = Gsl_eigen.symmv (`M(m)) in
   let m' = mm v (mm (diagOfVec l) (alloc_transpose v)) in
-  "symmEigs not giving column vectors" @? matrices_approximately_equal m m'
+  "symmEigs not giving column vectors" @? mat_approx_equal m m'
 
 let test_symmv _ =
   let b = Gsl_matrix.of_arrays [|[|-. 1.; 0.15|];[|0.15; -.2.|]|] in
