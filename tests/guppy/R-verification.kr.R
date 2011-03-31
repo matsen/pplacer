363923f3ac06bf4a956f7e9c1fde6f4c0bfa72c8
Erick Matsen <matsen@fhcrc.org>
2011-03-31 14:30:19 -0700

    Expanded guppy kr unit tests. Closes #22.
    The tests have been expanded, especially to include transforms.
    Organization is much improved.
    Now, weighted is covered by the precomputed tests, and unweighted by the
    matrix tests.

--- tests/guppy/R-verification.kr.R
+++ tests/guppy/R-verification.kr.R
@@ -22,10 +22,25 @@ answers
 
 
 # now say we have multiplicity 3 on one_x and multiplicity 5 on one_y
+
+no_trans = function (x) = x
 f = function (p) {
-  mx = (3/8)^p
-  my = (5/8)^p
-  outside(p, mx*1 + my*7 + 2)
+  g = function (mx,my) {
+    outside(p, (mx^p)*1 + (my^p)*7 + 2)
+  }
+  asx = asinh(3)
+  asy = asinh(5)
+  as_tot = asx + asy
+  c(
+   # here we have the multiplicity interpreted literally
+    g(3/8,5/8),
+   # hear each pquery gets one vote
+    g(1/2,1/2),
+   # and here is the asinh
+    g(asx/as_tot, asy/as_tot)
+  )
 }
-sapply(exponents,f)
+answers = rbind(sapply(exponents,f))
+rownames(answers) = c("no_trans", "unit_trans", "asinh_trans")
+answers
 
--- tests/guppy/test_kr_distance.ml
+++ tests/guppy/test_kr_distance.ml
@@ -1,3 +1,19 @@
+(* Unit testing for guppy kr.
+ *
+ * The testing has two components. First, there are the "predefined" tests where
+ * we have a fixed value we are aiming for. These are a combination of "simple"
+ * which have been computed by hand (actually calculated in
+ * R-verification.kr.R), and then "psbA" and "moran" which are pre-computed and
+ * we want to make sure they stay the same. All of those are with weighted
+ * placements, no multiplicity transformation, and use varying ps. Also within
+ * the predefined tests are some by hand values for various transformations of
+ * multiplicity.
+ *
+ * The second check are matrix tests which use an alternate method for computing
+ * KR. This is just for the unweighted case with no multiplicity and p=2.
+ *
+ *)
+
 open OUnit
 open Test_util
 
@@ -18,7 +34,7 @@ let predefined_tests transform_str which expected =
       let tree = Placerun.get_same_tree pr1 pr2 in
       let normalization = Gtree.tree_length tree in
       let calculated =
-        Kr_distance.dist_of_pres ~normalization transform p tree ~pre1 ~pre2 in
+        Kr_distance.scaled_dist_of_pres ~normalization transform p tree pre1 pre2 in
       (Printf.sprintf "%s x %s" pr_name1 pr_name2) >:: fun _ ->
         (Printf.sprintf "%f !~= %f" calculated expected) @? approx_equal expected calculated
     ) pairs;
@@ -34,25 +50,22 @@ let predefined_tests transform_str which expected =
  *  2   0.3678511 0.3333333 0.5400617
  *)
 
-let simple_no_trans_expected = [
+let simple_expected = [
   (0.5, [
     ("test1", "test2", 0.686887);
     ("test1", "test3", 0.319036);
     ("test2", "test3", 0.367851);
-    ("test4", "test4_demulti", 0.);
   ]);
   (* note that total tree length is 24 *)
   (1.0, [
     ("test1", "test2", 0.583333);
     ("test1", "test3", 0.25);
     ("test2", "test3", 0.333333);
-    ("test4", "test4_demulti", 0.);
   ]);
   (2.0, [
     ("test1", "test2", 0.677003);
     ("test1", "test3", 0.408248);
     ("test2", "test3", 0.540062);
-    ("test4", "test4_demulti", 0.);
   ]);
 ]
 
@@ -107,73 +120,46 @@ let moran_expected = [
 (*
  * from R-verification.kr.R:
 
-0.3394316 0.2812500 0.4506939
+                 [,1]      [,2]      [,3]
+no_trans    0.3394316 0.2812500 0.4506939
+unit_trans  0.3190356 0.2500000 0.4082483
+asinh_trans 0.3292013 0.2649481 0.4275588
 *)
-
-let simple_no_trans_expected = [
-  (0.5, [
-    ("test1", "test2", 0.686887);
-    ("test1", "test3", 0.319036);
-    ("test2", "test3", 0.367851);
-    ("test4", "test4_demulti", 0.);
-  ]);
-  (* note that total tree length is 24 *)
-  (1.0, [
-    ("test1", "test2", 0.583333);
-    ("test1", "test3", 0.25);
-    ("test2", "test3", 0.333333);
-    ("test4", "test4_demulti", 0.);
-  ]);
-  (2.0, [
-    ("test1", "test2", 0.677003);
-    ("test1", "test3", 0.408248);
-    ("test2", "test3", 0.540062);
-    ("test4", "test4_demulti", 0.);
-  ]);
-]
-
-let psbA_expected = [
-  (0.5, [
-    ("DCM", "coastal", 0.085899);
-    ("DCM", "surface", 0.0679409);
-    ("DCM", "upwelling", 0.0777173);
-    ("coastal", "surface", 0.0910193);
-    ("coastal", "upwelling", 0.0400257);
-    ("surface", "upwelling", 0.0850928);
-  ]);
-  (1.0, [
-    ("DCM", "coastal", 0.0341428);
-    ("DCM", "surface", 0.014255);
-    ("DCM", "upwelling", 0.0278099);
-    ("coastal", "surface", 0.032223);
-    ("coastal", "upwelling", 0.00951196);
-    ("surface", "upwelling", 0.0270792);
-  ]);
-  (2.0, [
-    ("DCM", "coastal", 0.120893);
-    ("DCM", "surface", 0.0396716);
-    ("DCM", "upwelling", 0.104636);
-    ("coastal", "surface", 0.0971287);
-    ("coastal", "upwelling", 0.0277832);
-    ("surface", "upwelling", 0.0827712);
-  ]);
-]
 
 let multi_notrans_expected =
   [
     0.5, [ "multi_test1", "test3", 0.3394316; ];
-    1.0, [ "multi_test1", "test3", 0.2812500; ];
+    1.0, [
+           "multi_test1", "test3", 0.2812500;
+           "test4", "test4_demulti", 0.;
+         ];
     2.0, [ "multi_test1", "test3", 0.4506939; ];
   ]
 
-let no_transform_predefined_suite =
+let multi_unit_expected =
+  [
+    0.5, [ "multi_test1", "test3", 0.3190356; ];
+    1.0, [ "multi_test1", "test3", 0.2500000; ];
+    2.0, [ "multi_test1", "test3", 0.4082483; ];
+  ]
+
+let multi_asinh_expected =
+  [
+    0.5, [ "multi_test1", "test3", 0.3292013; ];
+    1.0, [ "multi_test1", "test3", 0.2649481; ];
+    2.0, [ "multi_test1", "test3", 0.4275588; ];
+  ]
+
+let predefined_suite =
   List.map
     (fun (trans_str, n, pd) -> predefined_tests trans_str n pd)
     [
-      "no_trans", "simple", simple_no_trans_expected;
+      "no_trans", "simple", simple_expected;
       "no_trans", "psbA", psbA_expected;
       "no_trans", "moran", moran_expected;
-      "no_trans", "simple", multi_notrans_expected;
+      "no_trans", "multi", multi_notrans_expected;
+      "unit", "multi", multi_unit_expected;
+      "asinh", "multi", multi_asinh_expected;
     ]
 
 
@@ -222,6 +208,6 @@ let matrix_suite =
 
 (* *** the final suite *** *)
 let suite =
-  no_transform_predefined_suite
+  predefined_suite
   @ matrix_suite
 
