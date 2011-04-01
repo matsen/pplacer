db6f1b63ef864651c1bc197326865319bd6af7c4
Aaron Gallagher <habnabit@gmail.com>
2011-03-31 19:15:20 -0700

    Readding in the fantasy baseball functionality.
    This required some refactoring of Pplacer_run and Core, but I'm happy with the
    results of it. The code is better-organized now than it was. The only thing I'm
    slightly annoyed with is the pattern-matching required in the `nextfunc`
    implementations. This could be removed, but it would take more work than I
    really wanted to spend on it right now.

--- pplacer_src/core.ml
+++ pplacer_src/core.ml
@@ -17,27 +17,20 @@ let log_keep_factor = log keep_factor
 let final_tolerance = 1e-5
 
 type prior = Uniform_prior | Exponential_prior of float
+type result =
+  | Fantasy of (int * (float * float * float)) list
+  | Pquery of string * Pquery.pquery
 
 (* pplacer_core :
  * actually try the placements, etc. return placement records *)
-let pplacer_core prefs query_fname locs prior model ref_align gtree
-    ~darr ~parr ~snodes =
+let pplacer_core prefs locs prior model ref_align gtree ~darr ~parr ~snodes =
   let seq_type = Model.seq_type model
   and prior_fun =
     match prior with
       | Uniform_prior -> (fun _ -> 1.)
       | Exponential_prior mean ->
         fun pend -> Gsl_randist.exponential_pdf ~mu:mean pend
-  and ref_length = Alignment.length ref_align
-  and fantasy_mat =
-    if fantasy prefs <> 0. then
-      Fantasy.make_fantasy_matrix
-        ~max_strike_box:(int_of_float (strike_box prefs))
-        ~max_strikes:(max_strikes prefs)
-    else [||]
-  and fantasy_mod = Base.round (100. *. (fantasy_frac prefs))
-  and n_fantasies = ref 0
-  in
+  and ref_length = Alignment.length ref_align in
   let utilv_nsites = Gsl_vector.create ref_length in
   (* set up the number of pitches and strikes according to the prefs *)
   let (t_max_pitches, t_max_strikes) =
@@ -63,13 +56,11 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
   let process_query (query_name, query_seq) =
     Printf.printf ">%s\n" query_name;
     flush_all ();
-      (* we only proceed if fantasy baseball is turned off or if this is one of
-       * the sequences used for the fantasy baseball procedure *)
     if String.length query_seq <> ref_length then
       failwith ("query '"^query_name^"' is not the same length as the ref alignment");
-      (* prepare the query glv *)
+    (* prepare the query glv *)
     let query_arr = StringFuns.to_char_array query_seq in
-      (* the mask array shows true if it's included *)
+    (* the mask array shows true if it's included *)
     let informative c = c <> '?' && c <> '-' in
     let mask_arr = Array.map informative query_arr in
     let masked_query_arr =
@@ -83,13 +74,13 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
         | Alignment.Nucleotide_seq -> Array.map Nuc_models.lv_of_nuc a
         | Alignment.Protein_seq -> Array.map Prot_models.lv_of_aa a
     in
-      (* the query glv, which has been masked *)
+    (* the query glv, which has been masked *)
     let query_glv =
       Glv.lv_arr_to_constant_rate_glv
         (Model.n_rates model)
         (lv_arr_of_char_arr masked_query_arr)
     in
-      (* the full one, which will be used for the first stage only *)
+    (* the full one, which will be used for the first stage only *)
     Glv.prep_constant_rate_glv_from_lv_arr
       full_query_orig
       (lv_arr_of_char_arr query_arr);
@@ -98,22 +89,22 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
       ~dst:full_query_evolv
       ~src:full_query_orig
       (start_pend prefs);
-      (* make a masked alignment with just the given query sequence and the
-       * reference seqs *)
+    (* make a masked alignment with just the given query sequence and the
+     * reference seqs *)
     if write_masked prefs then
       Alignment.toFasta
         (Alignment.mask_align mask_arr
            (Alignment.stack [|query_name, query_seq|] ref_align))
         (query_name^".mask.fasta");
-      (* make our edges. we are breaking interface by making them then changing
-       * them later, but it would be silly to have setting functions for each
-       * edge. *)
+    (* make our edges. we are breaking interface by making them then changing
+     * them later, but it would be silly to have setting functions for each
+     * edge. *)
     let dist_edge = Glv_edge.make model (Glv.mimic query_glv) (start_pend prefs)
     and prox_edge = Glv_edge.make model (Glv.mimic query_glv) (start_pend prefs)
     and pend_edge = Glv_edge.make model query_glv (start_pend prefs)
     in
-      (* the h_r ranks the locations according to the h criterion. we use
-       * this as an ordering for the slower computation *)
+    (* the h_r ranks the locations according to the h criterion. we use
+     * this as an ordering for the slower computation *)
     let curr_time = Sys.time () in
     let h_r =
       List.sort
@@ -127,18 +118,18 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
     in
     if (verb_level prefs) >= 2 then Printf.printf "ranking took\t%g\n" ((Sys.time ()) -. curr_time);
     let h_ranking = List.map fst h_r in
-      (* first get the results from ML *)
+    (* first get the results from ML *)
     let curr_time = Sys.time () in
-      (* make our three taxon tree. we can't move this higher because the cached
-       * calculation on the edges needs to be the right size for the length of
-       * the query sequence. *)
+    (* make our three taxon tree. we can't move this higher because the cached
+     * calculation on the edges needs to be the right size for the length of
+     * the query sequence. *)
     let tt =
       Three_tax.make
         model
         utilv_nsites
         ~dist:dist_edge ~prox:prox_edge ~pend:pend_edge
     in
-      (* set tt edges to be for location loc with given pendant and distal branch lengths *)
+    (* set tt edges to be for location loc with given pendant and distal branch lengths *)
     let set_tt_edges loc ~pendant ~distal =
       let cut_bl = Gtree.get_bl gtree loc in
       let set_edge edge glv_arr len =
@@ -148,9 +139,9 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
           ~dst:(Glv_edge.get_orig edge);
         Glv_edge.set_bl model edge len
       in
-        (* still the same glv for query, but have to set branch length *)
+      (* still the same glv for query, but have to set branch length *)
       Glv_edge.set_bl model pend_edge pendant;
-        (* need to set the glv and branch length for dist and prox *)
+      (* need to set the glv and branch length for dist and prox *)
       set_edge dist_edge darr distal;
       set_edge prox_edge parr (cut_bl -. distal)
     in
@@ -166,9 +157,9 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
         ~pendant:(Placement.pendant_bl p)
         ~distal:(Placement.distal_bl p)
     in
-      (* prepare_tt: set tt up for loc. side effect! *)
+    (* prepare_tt: set tt up for loc. side effect! *)
     let prepare_tt loc = tt_edges_default loc in
-      (* evaluate the location wrt ML *)
+    (* evaluate the location wrt ML *)
     let ml_optimize_location mlo_tolerance loc =
       let () =
         try
@@ -184,20 +175,20 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
               query_name
               loc;
       in
-        (* get the results *)
+      (* get the results *)
       Three_tax.get_results tt
     in
     let safe_ml_optimize_location tol loc =
       try ml_optimize_location tol loc with
         | Gsl_error.Gsl_exn(_,_) -> begin
-            (* try again starting with default branch lengths *)
+          (* try again starting with default branch lengths *)
           tt_edges_default loc;
           ml_optimize_location tol loc
         end
     in
-      (* in play_ball we go down the h_ranking list and wait until we get
-       * strike_limit strikes, i.e. placements that are strike_box below the
-       * best one so far. *)
+    (* in play_ball we go down the h_ranking list and wait until we get
+     * strike_limit strikes, i.e. placements that are strike_box below the
+     * best one so far. *)
     let rec play_ball like_record n_strikes results = function
       | loc::rest -> begin
         try
@@ -208,17 +199,17 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
           if List.length results >= t_max_pitches then
             new_results
           else if like > like_record then
-              (* we have a new best likelihood *)
+            (* we have a new best likelihood *)
             play_ball like n_strikes new_results rest
           else if like < like_record-.(strike_box prefs) then
-              (* we have a strike *)
+            (* we have a strike *)
             if n_strikes+1 >= t_max_strikes then new_results
             else play_ball like_record (n_strikes+1) new_results rest
           else
-              (* not a strike, just keep on accumulating results *)
+            (* not a strike, just keep on accumulating results *)
             play_ball like_record n_strikes new_results rest
         with
-            (* we need to handle the exception here so that we continue the baseball recursion *)
+          (* we need to handle the exception here so that we continue the baseball recursion *)
           | Gsl_error.Gsl_exn(_,warn_str) ->
             Printf.printf "Warning: GSL problem with location %d for query %s; Skipped with warning \"%s\".\n" loc query_name warn_str;
             play_ball like_record n_strikes results rest
@@ -226,79 +217,79 @@ let pplacer_core prefs query_fname locs prior model ref_align gtree
       | [] -> results
     in
     let ml_results =
-        (* important to reverse for fantasy baseball. also should save time on sorting *)
+      (* important to reverse for fantasy baseball. also should save time on sorting *)
       List.rev (play_ball (-. infinity) 0 [] h_ranking)
     in
     if ml_results = [] then
       failwith
         (Printf.sprintf "empty results for %s!\n" query_name);
     if (verb_level prefs) >= 2 then Printf.printf "ML calc took\t%g\n" ((Sys.time ()) -. curr_time);
-    if fantasy prefs <> 0. then begin
-      Fantasy.add_to_fantasy_matrix ml_results fantasy_mat;
-      incr n_fantasies;
-    end;
+    if fantasy prefs <> 0. then
+      Fantasy ml_results
+    else begin
       (* these tuples are ugly but that way we don't need
        * to make a special type for ml results. *)
-    let get_like (_, (like, _, _)) = like in
-    let decreasing_cmp_likes r1 r2 =
-      - compare (get_like r1) (get_like r2) in
-    let sorted_ml_results =
-      List.sort decreasing_cmp_likes ml_results in
-    assert(sorted_ml_results <> []);
-    let best_like = get_like (List.hd sorted_ml_results) in
-    let keep_results, _ =
-      ListFuns.partitioni
-        (fun i r ->
-          ((i < keep_at_most) &&
-              (get_like r >= log_keep_factor +. best_like)))
-        sorted_ml_results
-    in
-      (* do final refinement of branch lengths *)
-    let refined_results =
-      List.map
-        (fun initial ->
-          let (loc, (_, pendant, distal)) = initial in
-          set_tt_edges loc ~pendant ~distal;
-          try
-            (loc, safe_ml_optimize_location final_tolerance loc)
-          with
-            | Gsl_error.Gsl_exn(_,warn_str) ->
-              Printf.printf "Warning: GSL problem with final branch length optimization for location %d. %s\n" loc warn_str;
-              initial)
-        keep_results
-    in
-    let sorted_ml_placements =
-      List.map2
-        (fun ml_ratio (loc, (log_like, pend_bl, dist_bl)) ->
-          Placement.make_ml
-            loc ~ml_ratio ~log_like ~pend_bl ~dist_bl)
-        (Base.ll_normalized_prob (List.map get_like refined_results))
-        refined_results
-    in
-    query_seq, Pquery.make_ml_sorted
-      ~namel:[query_name]
-      ~seq:query_seq
-      (if (calc_pp prefs) then begin
-          (* pp calculation *)
-        let curr_time = Sys.time () in
-          (* calculate marginal likes for those placements we will keep *)
-        let marginal_probs =
-          List.map
-            (fun placement ->
-              tt_edges_from_placement placement;
-              Three_tax.calc_marg_prob
-                prior_fun (pp_rel_err prefs) (max_pend prefs) tt)
-            sorted_ml_placements
-        in
-          (* add pp *)
-        if (verb_level prefs) >= 2 then Printf.printf "PP calc took\t%g\n" ((Sys.time ()) -. curr_time);
-        ((ListFuns.map3
-            (fun placement marginal_prob post_prob ->
-              Placement.add_pp placement ~marginal_prob ~post_prob)
-            sorted_ml_placements
-            marginal_probs
-            (Base.ll_normalized_prob marginal_probs)))
-      end
-       else sorted_ml_placements)
+      let get_like (_, (like, _, _)) = like in
+      let decreasing_cmp_likes r1 r2 =
+        - compare (get_like r1) (get_like r2) in
+      let sorted_ml_results =
+        List.sort decreasing_cmp_likes ml_results in
+      assert(sorted_ml_results <> []);
+      let best_like = get_like (List.hd sorted_ml_results) in
+      let keep_results, _ =
+        ListFuns.partitioni
+          (fun i r ->
+            ((i < keep_at_most) &&
+                (get_like r >= log_keep_factor +. best_like)))
+          sorted_ml_results
+      in
+    (* do final refinement of branch lengths *)
+      let refined_results =
+        List.map
+          (fun initial ->
+            let (loc, (_, pendant, distal)) = initial in
+            set_tt_edges loc ~pendant ~distal;
+            try
+              (loc, safe_ml_optimize_location final_tolerance loc)
+            with
+              | Gsl_error.Gsl_exn(_,warn_str) ->
+                Printf.printf "Warning: GSL problem with final branch length optimization for location %d. %s\n" loc warn_str;
+                initial)
+          keep_results
+      in
+      let sorted_ml_placements =
+        List.map2
+          (fun ml_ratio (loc, (log_like, pend_bl, dist_bl)) ->
+            Placement.make_ml
+              loc ~ml_ratio ~log_like ~pend_bl ~dist_bl)
+          (Base.ll_normalized_prob (List.map get_like refined_results))
+          refined_results
+      in
+      Pquery (query_seq, Pquery.make_ml_sorted
+        ~namel:[query_name]
+        ~seq:query_seq
+        (if (calc_pp prefs) then begin
+        (* pp calculation *)
+          let curr_time = Sys.time () in
+        (* calculate marginal likes for those placements we will keep *)
+          let marginal_probs =
+            List.map
+              (fun placement ->
+                tt_edges_from_placement placement;
+                Three_tax.calc_marg_prob
+                  prior_fun (pp_rel_err prefs) (max_pend prefs) tt)
+              sorted_ml_placements
+          in
+        (* add pp *)
+          if (verb_level prefs) >= 2 then Printf.printf "PP calc took\t%g\n" ((Sys.time ()) -. curr_time);
+          ((ListFuns.map3
+              (fun placement marginal_prob post_prob ->
+                Placement.add_pp placement ~marginal_prob ~post_prob)
+              sorted_ml_placements
+              marginal_probs
+              (Base.ll_normalized_prob marginal_probs)))
+        end
+         else sorted_ml_placements))
+    end
   in
   process_query
--- pplacer_src/pplacer_run.ml
+++ pplacer_src/pplacer_run.ml
@@ -229,9 +229,8 @@ let run_file prefs query_fname =
       ((Gtree.tree_length ref_tree) /.
         (float_of_int (Gtree.n_edges ref_tree)))
   in
-  let partial = Core.pplacer_core prefs query_fname locs prior
-    model ref_align ref_tree
-    ~darr ~parr ~snodes
+  let partial = Core.pplacer_core
+    prefs locs prior model ref_align ref_tree ~darr ~parr ~snodes
   in
   let n_done = ref 0 in
   let queries = List.length query_list in
@@ -240,33 +239,98 @@ let run_file prefs query_fname =
     Printf.printf "working on %s (%d/%d)...\n" query_name (!n_done) queries;
     flush_all ()
   in
-  let q = Queue.create () in
-  List.iter
-    (fun (name, seq) -> Queue.push (name, (String.uppercase seq)) q)
-    query_list;
-  let query_tbl = Hashtbl.create 1024 in
-  let gotfunc (seq, pq) =
-    Hashtbl.add query_tbl seq pq
-  and cachefunc (name, seq) =
-    match begin
-      try
-        Some (Hashtbl.find query_tbl seq)
-      with
-        | Not_found -> None
-    end with
-      | Some pq ->
-        let pq = Pquery.set_namel pq (name :: pq.Pquery.namel) in
-        incr n_done;
-        Hashtbl.replace query_tbl seq pq;
-        true
-      | None -> false
-  and progressfunc msg =
+  let progressfunc msg =
     if String.rcontains_from msg 0 '>' then begin
       let query_name = String.sub msg 1 ((String.length msg) - 1) in
       show_query query_name
     end else
       Printf.printf "%s\n" msg
   in
+  let q = Queue.create () in
+  List.iter
+    (fun (name, seq) -> Queue.push (name, (String.uppercase seq)) q)
+    query_list;
+
+  let gotfunc, cachefunc, donefunc = if Prefs.fantasy prefs <> 0. then begin
+    (* fantasy baseball *)
+    let fantasy_mat =
+      Fantasy.make_fantasy_matrix
+        ~max_strike_box:(int_of_float (Prefs.strike_box prefs))
+        ~max_strikes:(Prefs.max_strikes prefs)
+    and fantasy_mod = Base.round (100. *. (Prefs.fantasy_frac prefs))
+    and n_fantasies = ref 0
+    in
+    let gotfunc = function
+      | Core.Fantasy f ->
+        Fantasy.add_to_fantasy_matrix f fantasy_mat;
+        incr n_fantasies
+      | _ -> failwith "expected fantasy result"
+    and cachefunc _ =
+      (* if cachefunc returns true, the current thing is skipped; modulo
+       * division will be 0 for every N things *)
+      let res = (!n_done) mod fantasy_mod <> 0 in
+      incr n_done;
+      res
+    and donefunc () =
+      Fantasy.results_to_file
+        (Filename.basename (Filename.chop_extension query_fname))
+        fantasy_mat (!n_fantasies);
+      Fantasy.print_optimum fantasy_mat (Prefs.fantasy prefs) (!n_fantasies);
+    in gotfunc, cachefunc, donefunc
+
+  end else begin
+    (* not fantasy baseball *)
+    let query_tbl = Hashtbl.create 1024 in
+    let gotfunc = function
+      | Core.Pquery (seq, pq) -> Hashtbl.add query_tbl seq pq
+      | _ -> failwith "expected pquery result"
+    and cachefunc (name, seq) =
+      match begin
+        try
+          Some (Hashtbl.find query_tbl seq)
+        with
+          | Not_found -> None
+      end with
+        | Some pq ->
+          let pq = Pquery.set_namel pq (name :: pq.Pquery.namel) in
+          incr n_done;
+          Hashtbl.replace query_tbl seq pq;
+          true
+        | None -> false
+    and donefunc () =
+      let results = Hashtbl.fold
+        (fun _ pq l -> pq :: l)
+        query_tbl
+        []
+      in
+      let pr =
+        Placerun.make
+          ref_tree
+          query_bname
+          results
+      in
+      let final_pr =
+        if not (Refpkg.tax_equipped rp) then pr
+        else Refpkg.classify rp pr
+      and out_prefix = (Prefs.out_dir prefs)^"/"^(Placerun.get_name pr)
+      and invocation = (String.concat " " (Array.to_list Sys.argv))
+      in
+      if Prefs.legacy_place prefs then
+        Placerun_io.to_file
+          invocation
+          (out_prefix^".place")
+          final_pr
+      else if Prefs.csv prefs then
+        Placerun_io.to_csv_file (out_prefix^".csv") final_pr
+      else
+        Placerun_io.to_json_file
+          invocation
+          (out_prefix ^ ".json")
+          final_pr
+    in
+    gotfunc, cachefunc, donefunc
+
+  end in
   let rec nextfunc () =
     let x =
       try
@@ -282,35 +346,4 @@ let run_file prefs query_fname =
       (fun _ -> new pplacer_process partial gotfunc nextfunc progressfunc)
       (range (Prefs.children prefs)) in
   event_loop children;
-  let results = Hashtbl.fold
-    (fun _ pq l -> pq :: l)
-    query_tbl
-    []
-  in
-  let pr =
-    Placerun.make
-      ref_tree
-      query_bname
-      results
-  in
-  (* write output if we aren't in fantasy mode *)
-  if Prefs.fantasy prefs = 0. then begin
-    let final_pr =
-      if not (Refpkg.tax_equipped rp) then pr
-      else Refpkg.classify rp pr
-    and out_prefix = (Prefs.out_dir prefs)^"/"^(Placerun.get_name pr)
-    and invocation = (String.concat " " (Array.to_list Sys.argv))
-    in
-    if Prefs.legacy_place prefs then
-      Placerun_io.to_file
-        invocation
-        (out_prefix^".place")
-        final_pr
-    else if Prefs.csv prefs then
-      Placerun_io.to_csv_file (out_prefix^".csv") final_pr
-    else
-      Placerun_io.to_json_file
-        invocation
-        (out_prefix ^ ".json")
-        final_pr
-      end
+  donefunc ()
