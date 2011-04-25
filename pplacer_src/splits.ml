5ae9a3961f6aba4ac39c55867f7c67abe5f2b1df
Erick Matsen <matsen@fhcrc.org>
2011-04-25 07:35:55 -0700

    uniform_nonempty_partition works now

--- pplacer_src/splits.ml
+++ pplacer_src/splits.ml
@@ -63,29 +63,42 @@ let split_does_cut_lsetset split lsetset =
   Lsetset.cardinal lsetset <> Lsetset.cardinal (split_lsetset split lsetset)
 ;;
 
-(* Partition n_items into n_bins such that each bin is not empty. *)
-let uniform_nonempty_partition n_bins n_items =
+(* Throw n_items into n_bins such that each bin is not empty. *)
+let nonempty_balls_in_boxes rng ~n_bins ~n_items =
   assert(n_items >= n_bins);
-  let a = Array.create n_bins [] in
+  let counts = Array.create n_bins 1 in
+  for i=1 to n_items - n_bins do
+    let which_bin = Gsl_rng.uniform_int rng n_bins in
+    counts.(which_bin) <- counts.(which_bin) + 1
+  done;
+  Array.to_list counts
+;;
+nonempty_balls_in_boxes rng ~n_bins:5 ~n_items:10;;
+
+let lsetset_to_array lss =
+  let a = Array.make (Lsetset.cardinal lss) (Lsetset.choose lss) in
+  let _ = Lsetset.fold (fun x i -> a.(i) <- x; i+1) lss 0 in
   a
 ;;
 
-
-(* testing *)
-
-let sigma = auto_list_split [2;5;6];;
-let ls1 = LsetFuns.of_list [1;2];;
-let ls2 = LsetFuns.of_list [3;5;6];;
-let ls3 = LsetFuns.of_list [2;5];;
-let lss = LsetsetFuns.of_list [ls1; ls2];;
-split_lset sigma ls1;;
-let cut_lss = split_lsetset sigma lss;;
-
-split_does_cut_lset sigma ls2;;
-split_does_cut_lset sigma ls3;;
-split_does_cut_lsetset sigma lss;;
-split_does_cut_lsetset sigma cut_lss;;
-
+(* Throw the elements of lss into n_bins boxes, ensuring that each box has at
+ * least one element.
+ * The implementation below could be made more functional and list-y but I don't
+ * think that it would be shorter.
+ * *)
+let uniform_nonempty_partition rng n_bins lss =
+  let a = lsetset_to_array lss in
+  (* XXX *)
+  Guppy_kr.shuffle rng a;
+  let pos = ref 0 in
+  List.map
+    (fun n_samples ->
+      let new_lss =
+        LsetsetFuns.of_list (Array.to_list (Array.sub a !pos n_samples)) in
+      pos := !pos + n_samples;
+      new_lss)
+    (nonempty_balls_in_boxes rng ~n_bins ~n_items:(Lsetset.cardinal lss))
+;;
 
 
 (* convenience *)
@@ -112,4 +125,26 @@ let auto_list_split l =
   auto_split (LsetFuns.of_list l)
 ;;
 
+
+(* testing *)
+
+let rng = Gsl_rng.make (Gsl_rng.default ());;
+
+let sigma = auto_list_split [2;5;6];;
+let sigma' = auto_list_split [6];;
+let ls1 = LsetFuns.of_list [1;2];;
+let ls2 = LsetFuns.of_list [3;5;6];;
+let ls3 = LsetFuns.of_list [2;5];;
+let lss = LsetsetFuns.of_list [ls1; ls2];;
+split_lset sigma ls1;;
+let cut_lss = split_lsetset sigma lss;;
+
+split_does_cut_lset sigma ls2;;
+split_does_cut_lset sigma ls3;;
+split_does_cut_lsetset sigma lss;;
+split_does_cut_lsetset sigma cut_lss;;
+
+lsetset_to_array lss;;
+
+uniform_nonempty_partition rng 2 cut_lss;;
 
