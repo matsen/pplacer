59ba08aec16ccba787b6b040019b9cace7e947c3
Erick Matsen <matsen@fhcrc.org>
2011-05-25 07:01:02 -0700

    Clarifications for voronoi, not done yet though.

--- pplacer_src/voronoi.ml
+++ pplacer_src/voronoi.ml
@@ -1,8 +1,33 @@
-(* The algorithm is simple: first, with a two pass recursion, find the mark
- * label and distance to the closest leaf for all of the internal nodes. Now say
- * we are on an edge of length l bounded by internal nodes i_d (distal) and i_p
- * (proximal). If the mark label is the same for each of i_d and i_p, then there
- * are no marks inside the given edge. OTOH, if they are different and the
- * distances to the closest leaves are d_d and d_p, respectively, then we should
- * put a mark at (l + d_d - d_p)/2 with label taken from i_d.
+
+let mark_min m1 m2 = if (fst m1) <= (fst m2) then m1 else m2
+
+let distal_mark_map t =
+  (* recursion over tree, getting the (distance, color) from each internal node
+   * to its closest /distal/ leaf.
+   * *)
+
+let internal_mark_map t =
+  (* second recursion to find the (distance, color) from each internal node to
+   * its closest leaf. At each internal node, simply keep track of the smallest
+   * distance coming from above (can do options but I do think that seeding with
+   * infinity and an empty string should be fine here, as long as we check to
+   * make sure the eventual float is finite.)
+   * *)
+
+let update v above_cdist coptim =
+  (* Go down until we hit a mark or a truly colored leaf, then proceed back up
+   * the tree.
+   * *)
+
+let of_gtree t =
+  (* The algorithm is simple: first, with a two pass recursion, find the mark
+   * color and distance to the closest leaf for all of the internal nodes. Now
+   * say we are on an edge of length l bounded by internal nodes i_d (distal)
+   * and i_p (proximal). If the mark color is the same for each of i_d and i_p,
+   * then there are no marks inside the given edge. OTOH, if they are different
+   * and the distances to the closest leaves are d_d and d_p, respectively, then
+   * we should put a mark at (l + d_d - d_p)/2 with color taken from i_d.
  *)
+
+let remove_color v c =
+  (* We recur through the tree *)
--- pplacer_src/voronoi.mli
+++ pplacer_src/voronoi.mli
@@ -1,18 +1,44 @@
-(** Voronoi diagrams on phylogenetic tree.
+(** Voronoi diagrams for phylogenetic trees with input set leaves.
  *
- * "Marks" are where labels are changed in the diagram, and labels flow up the
- * tree. That is, a mark sets the label of all edges proximal to that mark until
+ * "Marks" are where colors are changed in the diagram, and colors flow up the
+ * tree. That is, a mark sets the color of all edges proximal to that mark until
  * another mark is encountered.
  *
- * The implementation will be set up for situations where we only have
- * at most one boundary per edge, as when the points of interest are nodes of
- * the phylogenetic tree.
+ * We only care about VDs when the set of interesting points are leaves or some
+ * subset thereof. Thus we have at most one boundary per edge, as when the
+ * points of interest are nodes of the phylogenetic tree.
  *
  *)
 
-type label = string
-type mark = float * label  (* The float is the distal branch length. *)
-type mark_map = mark IntMap.t
+module ColorMap = MapsSet.StringMap
+type color = string
+type cotree = Newick_gtree.t * color option IntMap.t
 
-val leaf_vd_of_gtree: Newick_gtree.t -> mark_map
+type mark =
+  {
+    edge_num: int;
+    distal_bl: float;
+    color: color;
+  }
+
+type cdist =
+  {
+    color: color;
+    distance: float;
+  }
+
+type v =
+  {
+    cot: cotree;
+    upper: (mark * (cdist option)) ColorMap.t; (* ? *)
+    lower: mark IntMap.t;
+    cdistm: cdist IntMap.t;
+  }
+
+val of_cotree: cotree -> t
 (** Compute the Voronoi diagram where points of interest are the leaves. *)
+
+val uncolor_leaf: v -> color -> v
+(** This function returns the updated Voronoi after removing the given color. *)
+
+
