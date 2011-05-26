cfe37c6cee6e323694c8761748ac22c19166c6a3
Erick Matsen <matsen@fhcrc.org>
2011-05-26 04:40:31 -0700

    some tune-ups-- internal nodes lose names and no deg-2 edges

--- pplacer_src/commiesim.ml
+++ pplacer_src/commiesim.ml
@@ -304,20 +304,28 @@ let main
   and new_bark = ref (Gtree.get_bark_map cluster_tree) in
   let rec aux = function
     | Stree.Node (i, subtrees) -> Stree.node i (List.map aux subtrees)
-    | Stree.Leaf i ->
+    | Stree.Leaf i as original ->
       let mult = int_of_float (Gtree.get_bl cluster_tree i) in
-      Stree.node i
-        (List.map
-           (fun e ->
-             let old_name = Gtree.get_name cluster_tree i in
-             incr next_id;
-             new_bark :=
-               Newick_bark.map_set_name
-               (!next_id)
-               (Printf.sprintf "%s_%d" old_name e)
-               (!new_bark);
-             Stree.leaf (!next_id))
-           (Base.range mult))
+      assert(mult >= 1);
+      if mult = 1 then original
+      else begin
+        let subtree =
+          Stree.node i
+            (List.map
+               (fun e ->
+                 let old_name = Gtree.get_name cluster_tree i in
+                 incr next_id;
+                 new_bark :=
+                   Newick_bark.map_set_name
+                   (!next_id)
+                   (Printf.sprintf "%s_%d" old_name e)
+                   (!new_bark);
+                 Stree.leaf (!next_id))
+               (Base.range mult))
+        in
+        new_bark := IntMap.remove i !new_bark;
+        subtree
+      end
   in
   let cluster_stree' = aux cluster_stree in
   let new_bark' = !new_bark in
