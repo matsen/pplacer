29ff65f10d473930aa0f2c3b685879c5228f7919
Aaron Gallagher <habnabit@gmail.com>
2011-06-17 11:15:04 -0700

    Refactoring `guppy fat` before averaging.

--- pplacer_src/guppy_fat.ml
+++ pplacer_src/guppy_fat.ml
@@ -24,41 +24,56 @@ object (self)
       | 0. -> fv total_width
       | x -> x *. (float_of_int total_multiplicity)
 
-  method action fnamel =
+  method private to_mass_pair tax_info pr =
     let transform, weighting, criterion = self#mass_opts in
-    let trees = List.map
-      (fun fname ->
-        let pr = Placerun_io.filtered_of_file fname in
-        let (tax_rp_opt, final_rt) = self#get_rpo_and_tree pr in
+    Mass_map.By_edge.of_placerun transform weighting criterion pr,
+    try
+      match tax_info with
+        | None -> None
+        | Some (taxt, ti_imap) ->
+          let massm =
+            Mass_map.By_edge.of_pre transform
+              (Tax_mass.pre (Gtree.top_id taxt) Placement.classif
+                 weighting criterion ti_imap pr)
+          in
+          Some (taxt, massm)
+    with
+      (* if we get a No_classif exception then don't make a tax fat tree *)
+      | Placement.No_classif -> None
+
+  method private to_fat_tree final_rt pr (phylo_mass, tax_mass) =
+    [
+      Some (pr.Placerun.name^".ref.fat"),
+      self#fat_tree_of_massm final_rt phylo_mass
+    ]
+    @
+      (match tax_mass with
+        | None -> []
+        | Some (taxt, massm) -> begin
+          let multiplier_override =
+            200. /. (Mass_map.By_edge.total_mass massm)
+          in
+          [
+            Some (pr.Placerun.name^".tax.fat"),
+            self#fat_tree_of_massm ~multiplier_override taxt massm
+          ]
+        end)
+
+  method action fnamel =
+    let prl = List.map Placerun_io.filtered_of_file fnamel in
+    self#check_placerunl prl;
+    let tax_info = match self#get_rpo with
+      | None -> None
+      | Some rp -> Some (Tax_gtree.of_refpkg_unit rp)
+    in
+    let mass_maps = List.map (self#to_mass_pair tax_info) prl in
+    let trees = List.map2
+      (fun pr pair ->
+        let _, final_rt = self#get_rpo_and_tree pr in
         pr.Placerun.name,
-        ([
-          Some (pr.Placerun.name^".ref.fat"),
-          self#fat_tree_of_massm final_rt
-            (Mass_map.By_edge.of_placerun transform weighting criterion pr)
-         ]
-         @
-           (try
-              match tax_rp_opt with
-                | None -> []
-                | Some rp -> begin
-                  let (taxt, ti_imap) = Tax_gtree.of_refpkg_unit rp in
-                  let massm =
-                    Mass_map.By_edge.of_pre transform
-                      (Tax_mass.pre (Gtree.top_id taxt) Placement.classif
-                         weighting criterion ti_imap pr)
-                  in
-                  let multiplier_override =
-                    200. /. (Mass_map.By_edge.total_mass massm)
-                  in
-                  [
-                    Some (pr.Placerun.name^".tax.fat"),
-                    self#fat_tree_of_massm ~multiplier_override taxt massm
-                  ]
-                end
-            with
-                (* if we get a No_classif exception then don't make a tax fat tree *)
-              | Placement.No_classif -> [])))
-      fnamel
+        self#to_fat_tree final_rt pr pair)
+      prl
+      mass_maps
     in
     match self#out_file_or_dir () with
       | Directory (dir, prefix) ->
