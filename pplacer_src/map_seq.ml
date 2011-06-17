abcf25474a79bdcdba9375fc811287f63ce123b6
Erick Matsen <matsen@fhcrc.org>
2011-06-17 13:31:13 -0700

    Map_seq.of_map actually right now

--- pplacer_src/map_seq.ml
+++ pplacer_src/map_seq.ml
@@ -1,6 +1,9 @@
 (* For calculating Maximum A Posteriori sequences for internal locations on the
  * tree. *)
 
+open Fam_batteries
+open MapsSets
+
 
 type pos = Distal | Proximal
 
@@ -9,13 +12,24 @@ type pos = Distal | Proximal
  * 1 leaf. *)
 
 let of_map u1 u2 model t ~darr ~parr m =
+  let code = match Model.seq_type model with
+  | Alignment.Nucleotide_seq -> Nuc_models.nuc_code
+  | Alignment.Protein_seq -> Prot_models.prot_code
+  in
+  let get_symbol i =
+    try code.(i) with | Invalid_argument _ -> assert(false)
+  in
+  let to_sym_str ind_arr =
+    StringFuns.of_char_array (Array.map get_symbol ind_arr)
+  in
   IntMap.mapi
-    (fun id, _ ->
-      Mutpick.get_summary
-        Mutpick.Proximal
-        Gsl_vector.max_index
-        (-1) u1 u2 model t
-        ~darr ~parr id)
+    (fun id _ ->
+      to_sym_str
+        (Mutpick.get_summary
+          Mutpick.Proximal
+          Gsl_vector.max_index
+          (-1) u1 u2 model t
+          ~darr ~parr id))
     m
 
 (* apply this one to the mrcam ... *)
