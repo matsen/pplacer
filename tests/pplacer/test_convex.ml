ebbe0f346556149acf00c6aec2ca5b05e9f10791
Aaron Gallagher <habnabit@gmail.com>
2011-06-22 15:21:24 -0700

    Adding tests for alternate_colors; simplifying `solve` test.

--- tests/pplacer/test_convex.ml
+++ tests/pplacer/test_convex.ml
@@ -2,22 +2,14 @@ open OUnit
 open Test_util
 
 open Convex
+open MapsSets
 
 let suite = List.map
   (fun (s, omega) ->
     let gt = Newick_gtree.of_string s in
     let st = gt.Gtree.stree
     and bm = gt.Gtree.bark_map in
-    let colors = MapsSets.IntMap.fold
-      (fun key value map ->
-        let name = value#get_name in
-        if name = "" then
-          map
-        else
-          MapsSets.IntMap.add key name map)
-      bm
-      MapsSets.IntMap.empty
-    in
+    let colors = IntMap.map (fun v -> v#get_name) bm in
     let _, calculated_omega = solve (colors, st) in
     let testfunc () =
       (Printf.sprintf "%d expected; got %d" omega calculated_omega) @? (omega = calculated_omega)
@@ -30,3 +22,16 @@ let suite = List.map
     "(((A,B),B),(A,A))", 4;
     "(A,(A,(B,C)))", 4;
   ]
+
+let suite =
+  ("alternate_colors" >::
+      fun () ->
+        let gt = Newick_gtree.of_string "(A,(A,(X,(B,B))))" in
+        let st = gt.Gtree.stree
+        and bm = gt.Gtree.bark_map in
+        let colors = IntMap.remove 2 (IntMap.map (fun v -> v#get_name) bm) in
+        let alt_colors = alternate_colors (colors, st) in
+        assert_equal (IntMap.nkeys alt_colors) 1;
+        assert_equal (IntMap.find 2 alt_colors) (ColorSet.of_list ["A"; "B"])
+  ) :: suite
+
