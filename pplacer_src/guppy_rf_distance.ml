da446d271e55e291d40de83d5adbd5c86a5130a3
Aaron Gallagher <habnabit@gmail.com>
2011-07-05 11:56:46 -0700

    Added an option to rf_distance for rooted comparisons.

--- pplacer_src/guppy_rf_distance.ml
+++ pplacer_src/guppy_rf_distance.ml
@@ -8,13 +8,40 @@ object (self)
   inherit subcommand () as super
   inherit output_cmd () as super_outfile
 
+  val rooted = flag "--rooted"
+    (Plain (false, "Compare trees rooted-ly instead of unrooted-ly."))
+
+  method specl = [
+    toggle_flag rooted;
+  ] @ super_outfile#specl
+
   method desc = "calculates RF distance of two trees"
   method usage = "usage: rf_distance <newick_trees>"
 
+  method private add_virtual_root gt =
+    let base_i = (Gtree.top_id gt) + 1 in
+    let gt' = Gtree.add_bark
+      base_i
+      (new Newick_bark.newick_bark
+         (`Of_bl_name_boot (None, Some "_virtual", None)))
+      gt
+    in
+    {gt' with Gtree.stree =
+        Stree.node
+          (base_i + 1)
+          [Stree.leaf base_i; Gtree.get_stree gt]}
+
+  method private of_file name =
+    let gt = Newick_gtree.of_file name in
+    if fv rooted then
+      self#add_virtual_root gt
+    else
+      gt
+
   method action = function
     | [tree1; tree2] ->
-      let tree1 = Newick_gtree.of_file tree1
-      and tree2 = Newick_gtree.of_file tree2 in
+      let tree1 = self#of_file tree1
+      and tree2 = self#of_file tree2 in
       let to_strset tree =
         Strsplits.strset_of_gtree_and_lset
           tree
