39c8bdd7d18b4fde28b6652b632ce61d1bb95625
Erick Matsen <matsen@fhcrc.org>
2011-07-07 09:57:24 -0700

    bumping version number to correct value

--- pplacer_src/version.ml
+++ pplacer_src/version.ml
@@ -1,6 +1,6 @@
 let chop_revision s = Str.replace_first (Str.regexp "\\.[a-z]+[0-9]+") "" s
 
-let version_revision = "v1.1.alpha08"
+let version_revision = "v1.1.alpha09"
 let version = chop_revision version_revision
 
 
