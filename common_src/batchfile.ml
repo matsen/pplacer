fe027a24c87a8197db2fa49323649472e2b28412
Aaron Gallagher <habnabit@gmail.com>
2011-07-17 12:43:17 -0700

    Adding substitution to batchfiles.

--- common_src/batchfile.ml
+++ common_src/batchfile.ml
@@ -1,3 +1,7 @@
+open MapsSets
+
+let compose f g a = f (g a)
+
 let batchfile_regexp = Str.regexp begin
   String.concat "\\|" [
     (* whitespace (ignored) *)
@@ -54,3 +58,28 @@ let of_file fname =
   let lines = File_parsing.string_list_of_file fname in
   let tokens = Base.map_and_flatten tokenize_batchfile lines in
   parse tokens
+
+let placeholder_regexp = Str.regexp "{\\([a-zA-Z0-9_-]+\\)}"
+let substitute_placeholders m =
+  let nice_find s =
+    try
+      StringMap.find s m
+    with Not_found ->
+      failwith ("unspecified batchfile substitution: " ^ s)
+  in
+  Str.global_substitute
+    placeholder_regexp
+    (compose nice_find (Str.matched_group 1))
+
+let argument_regexp = Str.regexp "^\\([a-zA-Z0-9_-]+\\)=\\(.*\\)$"
+let split_arguments l =
+  List.fold_left
+    (fun accum s ->
+      if not (Str.string_match argument_regexp s 0) then
+        failwith ("malformed batchfile argument: " ^ s);
+      StringMap.add
+        (Str.matched_group 1 s)
+        (Str.matched_group 2 s)
+        accum)
+    StringMap.empty
+    l
--- common_src/subcommand.ml
+++ common_src/subcommand.ml
@@ -100,7 +100,9 @@ let cmd_map_of_list l =
 
 (* intended to be the inner loop of a function *)
 let rec inner_loop ~prg_name ~version (display_map, cmd_map) =
-  let process = process_cmd prg_name display_map cmd_map in
+  let process = process_cmd prg_name display_map cmd_map
+  and args = ref []
+  and batchfile = ref None in
   Arg.parse
     [
       "--version", Arg.Unit (fun () -> print_endline version; exit 0),
@@ -108,17 +110,22 @@ let rec inner_loop ~prg_name ~version (display_map, cmd_map) =
       "--cmds", Arg.Unit (fun () -> print_avail_cmds prg_name display_map),
       "Print a list of the available commands.";
       "--batch", Arg.String (fun fname ->
-        let argll = Batchfile.of_file fname in
-        List.iter process argll),
+        batchfile := Some (Batchfile.of_file fname)),
       "Run the provided batch file of guppy commands";
     ]
-    (fun _ -> (* anonymous args. tl to remove command name. *)
-      process (List.tl (Array.to_list Sys.argv));
-      exit 0) (* need to exit to avoid processing the other anon args as cmds *)
+    (fun arg -> args := arg :: !args)
     (Printf.sprintf
       "Type %s --cmds to see the list of available commands."
-      prg_name)
-
+      prg_name);
+  match !batchfile with
+    | None -> process (List.rev !args)
+    | Some argll ->
+      let substitutions = Batchfile.split_arguments !args in
+      let argll' = List.map
+        (List.map (Batchfile.substitute_placeholders substitutions))
+        argll
+      in
+      List.iter process argll'
 
 (* the new stuff *)
 exception No_default of string * string
