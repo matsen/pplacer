665433d191c7f44c2c484a26924d6cf4d6b483af
Aaron Gallagher <habnabit@gmail.com>
2011-07-22 14:42:28 -0700

    Adding `rppr info`.

--- pplacer_src/rppr_commands.ml
+++ pplacer_src/rppr_commands.ml
@@ -7,5 +7,6 @@ let command_list () =
       "ref_tree", (fun () -> new Rppr_ref_tree.cmd ());
       "voronoi", (fun () -> new Rppr_voronoi.cmd ());
       "prunetre", (fun () -> new Rppr_prunetre.cmd ());
+      "info", (fun () -> new Rppr_info.cmd ());
     ];
   ]
