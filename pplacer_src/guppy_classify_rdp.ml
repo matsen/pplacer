0c3b2f1cae5654776dda5776ca4a8bac5fa886ac
Aaron Gallagher <habnabit@gmail.com>
2011-08-03 11:06:14 -0700

    x

--- pplacer_src/guppy_classify_rdp.ml
+++ pplacer_src/guppy_classify_rdp.ml
@@ -1,17 +1,18 @@
+open Batteries
 open Subcommand
 open Guppy_cmdobjs
-
-let tab = Str.regexp "\t"
+open MapsSets
 
 class cmd () =
 object (self)
   inherit subcommand () as super
   inherit refpkg_cmd ~required:true as super_refpkg
+  inherit sqlite_cmd () as super_sqlite
 
   val csv_out = flag "--csv"
     (Plain (false, "Write .class.csv files containing CSV data."))
 
-  method specl = toggle_flag csv_out :: super_refpkg#specl
+  method specl = toggle_flag csv_out :: super_refpkg#specl @ super_sqlite#specl
 
   method desc = "converts RDP output to something resmbling guppy classify output"
   method usage = "usage: classify_rdp -c some.refpkg rdp_output_file[s]"
@@ -19,53 +20,86 @@ object (self)
   method action argl =
     let rp = self#get_rp in
     let td = Refpkg.get_taxonomy rp in
-    let name_map = Tax_id.TaxIdMap.fold
-      (fun tax_id name ->
-        MapsSets.StringMap.add
-          name
-          (Tax_id.to_string tax_id))
-      td.Tax_taxonomy.tax_name_map
-      MapsSets.StringMap.empty
+    let name_map = Tax_id.TaxIdMap.enum td.Tax_taxonomy.tax_name_map
+      |> Enum.map (curry identity |> flip |> uncurry |- second Tax_id.to_string)
+      |> StringMap.of_enum
+    and sqlite_out = match !(sqlite_fname.Subcommand.value) with
+      | Some _ -> true
+      | None -> false
     in
 
-    let out_func name outal =
-      if fv csv_out then
-        let ch = open_out (name ^ ".class.csv") in
-        output_string ch "name,desired_rank,rank,tax_id,likelihood,origin\n";
-        List.iter
-          (fun arr -> Printf.fprintf ch
-            "%s,%s\n"
-            (String.concat "," (Array.to_list arr))
-            name)
-          outal;
-        close_out ch
-      else
-        let ch = open_out (name ^ ".class.tab") in
-        String_matrix.write_padded ch (Array.of_list outal);
-        close_out ch
-    in
+    let out_func =
+      if sqlite_out then begin
+        let db = self#get_db in
+        let close () =
+          Sql.check_exec db "COMMIT";
+          Sql.close db
+        and process name =
+          Sql.check_exec db "BEGIN TRANSACTION";
+          let pn_st = Sqlite3.prepare db
+            "INSERT INTO placement_names VALUES (?, ?, ?);"
+          and pc_st = Sqlite3.prepare db
+            "INSERT INTO placement_classifications VALUES (?, ?, ?, ?, ?)"
+          in
 
-    List.iter
-      (fun rdpfile ->
-        let lines = File_parsing.string_list_of_file rdpfile in
-        let lines' = Base.map_and_flatten
-          (fun line ->
-            (* past participle of 'to split' *)
-            let splut = Array.of_list (Str.split tab line) in
-            List.fold_left
-              (fun accum idx ->
+          Sql.check_exec db "INSERT INTO placements VALUES (NULL)";
+          let place_id = Sqlite3.last_insert_rowid db in
+          Enum.iter
+            (fun arr ->
+              Sql.bind_step_reset db pn_st
                 [|
-                  splut.(0);
-                  splut.(idx + 1);
-                  splut.(idx + 1);
-                  (MapsSets.StringMap.find splut.(idx) name_map);
-                  splut.(idx + 2);
-                |] :: accum)
-              []
-              [8; 11; 14; 17; 20]
-          )
-          lines
+                  Sql.D.INT place_id;
+                  Sql.D.TEXT arr.(0);
+                  Sql.D.TEXT name;
+                |];
+              Sql.bind_step_reset db pc_st
+                [|
+                  Sql.D.INT place_id;
+                  Sql.D.TEXT arr.(1);
+                  Sql.D.TEXT arr.(2);
+                  Sql.D.TEXT arr.(3);
+                  Sql.D.TEXT arr.(4);
+                |])
         in
-        out_func rdpfile lines')
-      argl
+        finally close process
+
+      end else if fv csv_out then begin fun name ->
+        let ch = Legacy.open_out (name ^ ".class.csv") in
+        Csv.save_out
+          ch
+          [["name"; "desired_rank"; "rank"; "tax_id"; "likelihood"; "origin"]];
+        Enum.map Array.to_list
+          |- List.of_enum
+          |- Csv.save_out ch
+          |> finally (fun () -> Legacy.close_out ch)
+
+      end else begin fun name ->
+        let ch = Legacy.open_out (name ^ ".class.tab") in
+        Array.of_enum
+          |- String_matrix.write_padded ch
+          |> finally (fun () -> Legacy.close_out ch)
+
+      end
+
+    and classify line =
+      (* past participle of 'to split' *)
+      let splut = String.nsplit line "\t" |> Array.of_list in
+      List.fold_left
+        (fun accum idx ->
+          [|
+            splut.(0);
+            splut.(idx + 1);
+            splut.(idx + 1);
+            (MapsSets.StringMap.find splut.(idx) name_map);
+            splut.(idx + 2);
+          |] :: accum)
+        []
+        [8; 11; 14; 17; 20]
+
+    in
+    List.enum argl
+      |> Enum.map
+          (identity &&& (File.lines_of |- Enum.map (classify |- List.enum) |- Enum.flatten))
+      |> Enum.iter (uncurry out_func)
+
 end
