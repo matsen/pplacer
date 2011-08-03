e1d202ca3d3ea9db0abde330fb8249cc6cdcb4cf
Aaron Gallagher <habnabit@gmail.com>
2011-08-03 12:20:09 -0700

    z

--- pplacer_src/guppy_classify_rdp.ml
+++ pplacer_src/guppy_classify_rdp.ml
@@ -29,79 +29,71 @@ object (self)
     in
 
     let out_func =
-      if sqlite_out then begin
+      (* if sqlite_out then begin *)
         let db = self#get_db in
+        Sql.check_exec db "BEGIN TRANSACTION";
+        let pn_st = Sqlite3.prepare db
+          "INSERT INTO placement_names VALUES (?, ?, ?);"
+        and pc_st = Sqlite3.prepare db
+          "INSERT INTO placement_classifications VALUES (?, ?, ?, ?, ?)"
+        in
         let close () =
           Sql.check_exec db "COMMIT";
           Sql.close db
-        and process name =
-          Sql.check_exec db "BEGIN TRANSACTION";
-          let pn_st = Sqlite3.prepare db
-            "INSERT OR IGNORE INTO placement_names VALUES (?, ?, ?);"
-          and pc_st = Sqlite3.prepare db
-            "INSERT INTO placement_classifications VALUES (?, ?, ?, ?, ?)"
-          in
-
+        and process origin name =
           Sql.check_exec db "INSERT INTO placements VALUES (NULL)";
           let place_id = Sqlite3.last_insert_rowid db in
-          Enum.iter
-            (fun arr ->
-              Sql.bind_step_reset db pn_st
-                [|
-                  Sql.D.INT place_id;
-                  Sql.D.TEXT arr.(0);
-                  Sql.D.TEXT name;
-                |];
-              Sql.bind_step_reset db pc_st
-                [|
-                  Sql.D.INT place_id;
-                  Sql.D.TEXT arr.(1);
-                  Sql.D.TEXT arr.(2);
-                  Sql.D.TEXT arr.(3);
-                  Sql.D.TEXT arr.(4);
-                |])
+          Sql.bind_step_reset db pn_st
+            [|
+              Sql.D.INT place_id;
+              Sql.D.TEXT name;
+              Sql.D.TEXT origin;
+            |];
+          List.iter
+            (List.map (fun x -> Sql.D.TEXT x)
+             |- Array.of_list
+             |- Array.append [| Sql.D.INT place_id |]
+             |- Sql.bind_step_reset db pc_st)
+
         in
         finally close process
 
-      end else if fv csv_out then begin fun name ->
-        let ch = Legacy.open_out (name ^ ".class.csv") in
-        Csv.save_out
-          ch
-          [["name"; "desired_rank"; "rank"; "tax_id"; "likelihood"; "origin"]];
-        Enum.map Array.to_list
-          |- List.of_enum
-          |- Csv.save_out ch
-          |> finally (fun () -> Legacy.close_out ch)
+      (* end else if fv csv_out then begin fun name -> *)
+      (*   let ch = Legacy.open_out (name ^ ".class.csv") in *)
+      (*   Csv.save_out *)
+      (*     ch *)
+      (*     [["name"; "origin"; "desired_rank"; "rank"; "tax_id"; "likelihood"]]; *)
+      (*   Enum.map Array.to_list *)
+      (*     |- List.of_enum *)
+      (*     |- Csv.save_out ch *)
+      (*     |> finally (fun () -> Legacy.close_out ch) *)
 
-      end else begin fun name ->
-        let ch = Legacy.open_out (name ^ ".class.tab") in
-        Array.of_enum
-          |- String_matrix.write_padded ch
-          |> finally (fun () -> Legacy.close_out ch)
+      (* end else begin fun name -> *)
+      (*   let ch = Legacy.open_out (name ^ ".class.tab") in *)
+      (*   Array.of_enum *)
+      (*     |- String_matrix.write_padded ch *)
+      (*     |> finally (fun () -> Legacy.close_out ch) *)
 
-      end
+      (* end *)
 
     and classify line =
       (* past participle of 'to split' *)
       let splut = String.nsplit line "\t" |> Array.of_list in
-      List.fold_left
+      splut.(0), List.fold_left
         (fun accum idx ->
-          [|
-            splut.(0);
+          [
             splut.(idx + 1);
             splut.(idx + 1);
             (MapsSets.StringMap.find splut.(idx) name_map);
             splut.(idx + 2);
-          |] :: accum)
+          ] :: accum)
         []
         [8; 11; 14; 17; 20]
 
     in
     List.enum argl
       |> Enum.map
-          (identity &&& (File.lines_of |- Enum.map (classify |- List.enum) |- Enum.flatten))
-      |> Enum.map
-          (fun (name, enum) -> name, Enum.map (flip Array.append [| name |]) enum)
-      |> Enum.iter (uncurry out_func)
+          (identity &&& (File.lines_of |- Enum.map classify))
+      |> Enum.iter (fun (a, bcl) -> Enum.iter (out_func a |> uncurry) bcl)
 
 end
