806be68eed99b49d96becb8e6153a1e323119add
Erick Matsen <matsen@fhcrc.org>
2011-08-10 17:13:32 -0700

    adding on rewinding for priors with support near origin

--- pplacer_src/three_tax.ml
+++ pplacer_src/three_tax.ml
@@ -115,25 +115,40 @@ let write_like_surf prior max_pend tt fname n_samples =
 (* Find an appropriate upper limit for pendant branch length integration. If we
  * come up with a resonable upper limit then we get better results than
  * integrating out to max_pend.
- * This function uses the current pend branch length and moves right in
- * increments of the current branch length, stopping when the ll function drops
- * below 1e-10*orig_ll.
  * Note 1: this does change the pend_bl.
- * Note 2: this assumes that the likelihood function is monotonic in the pendant
- * branch length.
+ * Note 2: this assumes that the likelihood function times the prior is
+ * monotonically decreasing in the pendant branch length beyond the original
+ * branch length, (in our applications, the original branch length is the ML
+ * branch length, and the prior is monotonically decreasing.)
+ * Note 3: we assume that the prior is monotonically decreasing in pendant
+ * branch length (yes for us).
  *)
 let find_upper_limit max_pend prior orig_ll tt =
-  let log_prior x = log (prior x)
-  and orig_pend_bl = get_pend_bl tt
-  in
-  let log_cutoff = -10. +. orig_ll +. (log_prior orig_pend_bl) in
-  let rec aux next_pend_bl =
-    set_pend_bl tt next_pend_bl;
-    if log_cutoff > (log_like tt) +. (log_prior next_pend_bl) then get_pend_bl tt
-    else if next_pend_bl > max_pend then max_pend
-    else aux (orig_pend_bl +. next_pend_bl)
-  in
-  aux (get_pend_bl tt)
+  let orig_pend_bl = get_pend_bl tt in
+  if prior orig_pend_bl = 0. then begin
+    (* Prior is zero. We just rewind to someplace reasonably small where the
+     * prior is still below some small quantity. *)
+    let rec aux curr_pend_bl =
+      let next_pend_bl = curr_pend_bl /. 2. in
+      if prior next_pend_bl > exp (-10.) then curr_pend_bl else aux next_pend_bl
+    in
+    aux orig_pend_bl
+  end
+  else begin
+    (* Move right uses the current pend branch length and moves right in
+      * increments of the current branch length, stopping when the ll function
+      * drops 10 log units. *)
+    let log_prior x = log (prior x) in
+    let log_cutoff = -10. +. orig_ll +. (log_prior orig_pend_bl) in
+    let rec aux next_pend_bl =
+      set_pend_bl tt next_pend_bl;
+      if log_cutoff > (log_like tt) +. (log_prior next_pend_bl) then
+        next_pend_bl
+      else if next_pend_bl > max_pend then max_pend
+      else aux (orig_pend_bl +. next_pend_bl)
+    in
+    aux (get_pend_bl tt)
+  end
 
 (* The idea here is to properly integrate log likelihood functions by removing
  * some portion so that when we actually do the integration, we don't have
