49e35964bd9276288f966da15985e856de93d8f0
Aaron Gallagher <habnabit@gmail.com>
2011-09-02 17:09:32 -0700

    Fixing up Matrix to be a superset of Bigarray and Gsl_matrix both.

--- pplacer_src/matrix.ml
+++ pplacer_src/matrix.ml
@@ -1,11 +1,21 @@
 open Ppatteries
+include Bigarray.Array2
 include Gsl_matrix
 
-let n_rows = dims |- fst
-let n_cols = dims |- snd
+let create a b = Bigarray.Array2.create Bigarray.float64 Bigarray.c_layout a b
+let mimic a = create (dim1 a) (dim2 a)
+
+(* Get the "worst" floating point classification in the tensor.
+ * Note:
+# max FP_normal FP_subnormal;;
+- : fpclass = FP_subnormal
+ * *)
+let fp_classify x =
+  enum x
+  |> Enum.fold (fun worst flt -> max worst (classify_float flt)) FP_normal
 
 let ppr ff m =
-  let last = n_rows m - 1 in
+  let last = dim1 m - 1 in
   Format.fprintf ff "@[{";
   for i=0 to last do
     Linear_utils.ppr_gsl_vector ff (row m i);
