8372f565d2afa345b6fb4d6683950c90a7e62770
Aaron Gallagher <habnabit@gmail.com>
2011-10-07 18:20:56 -0400

    Fixing `guppy kr_heat` for the updated Kr_distance.

--- pplacer_src/guppy_kr_heat.ml
+++ pplacer_src/guppy_kr_heat.ml
@@ -8,9 +8,9 @@ let transport_map t pre1 pre2 =
     IntMap.map
     (* we don't care about where we are along the edge *)
       (List.map snd)
-      (Kr_distance.make_kr_map
-        (Mass_map.Indiv.of_pre pre1)
-        (Mass_map.Indiv.of_pre pre2)) in
+      (Kr_distance.make_n_kr_map
+        [Mass_map.Indiv.of_pre pre1;
+         Mass_map.Indiv.of_pre pre2]) in
   let sum_over_krs_of_id id =
     List.fold_right
       (fun kr_v -> ( +. ) (kr_v.(0) -. kr_v.(1)))
