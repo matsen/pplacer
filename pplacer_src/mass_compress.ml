29b03ef2030b822220955d06d199e4fae36c332d
Aaron Gallagher <habnabit@gmail.com>
2011-10-14 14:38:02 -0700

    Resolving XXX comments in Mass_compress.
    The amount of mass before and after running `guppy compress` was verified to be
    the same with some quick tests, but some actual unit tests will get added.

--- pplacer_src/mass_compress.ml
+++ pplacer_src/mass_compress.ml
@@ -10,36 +10,36 @@ let of_placerun ?(p = 1.) ~c weighting criterion pr =
   |> List.map (fun (_, pql) ->
     let uptri = List.map mass_of_pq pql
       |> Kr_distance.multi_dist gt p
-    and pqa = Array.of_list pql in
-    let l = Array.length pqa |> pred
+    and pqa = Array.of_list pql
     and nodem = ref IntMap.empty in
-    (* XXX: figure out what should happen to isolated nodes which don't get
-     * added to any part of the map *)
-    for i = 0 to l do
-      for j = 0 to l do
-        match i, j with
-          | i, j when i = j -> ()
-          | i, j when Uptri.get_loose uptri i j > c -> ()
-          | i, j -> nodem := IntMap.add_listly i j !nodem
-      done;
-    done;
-    let rec aux accum nodem =
+    Uptri.iterij
+      (fun i j v ->
+        if i = j || v > c then () else (* ... *)
+        IntMap.add_listly i j !nodem
+          |> IntMap.add_listly j i
+          |> (:=) nodem)
+      uptri;
+    let rec ret = Array.filteri (fun i _ -> not (IntMap.mem i !nodem)) pqa
+      |> Array.to_list
+    and aux accum used nodem =
       if IntMap.is_empty nodem then accum else (* ... *)
       let w, xs = IntMap.enum nodem |> Enum.arg_max (snd |- IntSet.cardinal) in
       if IntSet.is_empty xs then accum else (* ... *)
-      let all_touching = IntSet.add w xs in
-      let accum' =
-        IntSet.elements xs
+      let all_touched = IntSet.add w xs in
+      let accum' = IntSet.diff all_touched used
+        |> IntSet.elements
         |> List.map (Array.get pqa)
-        |> Pquery.merge_into pqa.(w)
-        |> flip List.cons accum
+        |> junction
+            List.is_empty
+            (const accum)
+            (Pquery.merge |- flip List.cons accum)
+      and used' = IntSet.union all_touched used
       and nodem' = IntMap.add w IntSet.empty nodem
         |> IntSet.fold
-            (* XXX: do we really want to remove all of these, or just w? *)
-            (flip IntMap.modify (flip IntSet.diff all_touching))
+            (flip IntMap.modify (IntSet.remove w))
             xs
       in
-      aux accum' nodem'
+      aux accum' used' nodem'
     in
-    IntMap.map IntSet.of_list !nodem |> aux [])
+    IntMap.map IntSet.of_list !nodem |> aux ret IntSet.empty)
   |> List.flatten
