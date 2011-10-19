cd3d7bbf16d3918f1d130fc829b62f9bf169b17a
Aaron Gallagher <habnabit@gmail.com>
2011-10-18 17:23:42 -0700

    Adding type="confidence" to confidence phyloxml tags.

--- pplacer_src/newick_bark.ml
+++ pplacer_src/newick_bark.ml
@@ -87,7 +87,10 @@ class newick_bark arg =
       []
       |> maybe_map_cons (Myxml.tag "name") name
       |> maybe_map_cons (Printf.sprintf "%g" |- Myxml.tag "branch_length") bl
-      |> maybe_map_cons (Printf.sprintf "%g" |- Myxml.tag "confidence") confidence
+      |> maybe_map_cons
+          (Printf.sprintf "%g"
+           |- Myxml.tag "confidence" ~attributes:["type", "confidence"])
+          confidence
       |> List.rev
     end
 
