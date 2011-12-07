99c3300d809fe76db05c7eae5b5f16355c5103ba
Aaron Gallagher <habnabit@gmail.com>
2011-12-07 11:31:40 -0800

    Adding an --edpl flag to `guppy fat`.

--- pplacer_src/edpl.ml
+++ pplacer_src/edpl.ml
@@ -22,3 +22,17 @@ let of_pquery criterion rdist_uptri pq =
   in
   2. *. (aux 0. pq.Pquery.place_list)
 
+let average fl =
+  List.fsum fl /. float_of_int (List.length fl)
+
+let map_of_placerun criterion pr =
+  let dm = Placerun.get_ref_tree pr |> Edge_rdist.build_pairwise_dist in
+  List.fold_left
+    (fun accum pq ->
+      IntMap.add_listly
+        (Pquery.best_location criterion pq)
+        (of_pquery criterion dm pq)
+        accum)
+    IntMap.empty
+    (Placerun.get_pqueries pr)
+  |> IntMap.map average
