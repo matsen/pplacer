bb37a5b370a3ac8a4f224e3cad7ff25c953cf9d2
Aaron Gallagher <habnabit@gmail.com>
2011-12-23 16:00:17 -0800

    Removing the old dedup file format entirely.

--- pplacer_src/guppy_redup.ml
+++ pplacer_src/guppy_redup.ml
@@ -12,12 +12,9 @@ object (self)
 
   val dupfile = flag "-d"
     (Needs_argument ("dupfile", "The dedup file to use to restore duplicates."))
-  val multi_dupfile = flag "-m"
-    (Plain (false, "If specified, redup with counts instead of a name list."))
 
   method specl = super_output#specl @ [
     string_flag dupfile;
-    toggle_flag multi_dupfile;
   ]
 
   method desc = "restores duplicates to deduped placefiles"
@@ -25,37 +22,21 @@ object (self)
 
   method private placefile_action = function
     | [pr] ->
-      let pr' =
-        if fv multi_dupfile then
-          let sequence_tbl = Hashtbl.create 1024 in
-          let f = function
-            | [canon; rep; count] ->
-              Hashtbl.add sequence_tbl canon (rep, float_of_string count)
-            | _ -> failwith "malformed row in jplace"
-          in
-          fv dupfile
-            |> open_in
-            |> csv_in_channel
-            |> Csv.of_in_obj
-            |> Csv.iter ~f;
-          Placerun.redup sequence_tbl pr
-        else
-          let sequence_tbl = Hashtbl.create 1024 in
-          fv dupfile
-            |> File.lines_of
-            |> Enum.map (Str.split white_regexp |- (List.hd &&& identity))
-            |> Enum.iter
-                (fun (k, vs) ->
-                  List.iter
-                    (identity &&& const 1. |- Hashtbl.add sequence_tbl k)
-                    vs);
-          Placerun.redup sequence_tbl pr
-
+      let sequence_tbl = Hashtbl.create 1024 in
+      let f = function
+        | [canon; rep; count] ->
+          Hashtbl.add sequence_tbl canon (rep, float_of_string count)
+        | _ -> failwith "malformed row in jplace"
       in
-      self#write_placefile
-        "guppy redup"
-        (self#single_file ())
-        pr'
+      fv dupfile
+        |> open_in
+        |> csv_in_channel
+        |> Csv.of_in_obj
+        |> Csv.iter ~f;
+      Placerun.redup sequence_tbl pr
+        |> self#write_placefile
+            "guppy redup"
+            (self#single_file ())
 
     | l ->
       List.length l
