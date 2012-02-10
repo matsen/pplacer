3b0f7d904120a42a542b4396b1443c92da408311
Aaron Gallagher <habnabit@gmail.com>
2012-02-10 14:37:20 -0800

    Appropriately scaling mass across names in Pquery.set_mass.

--- pplacer_src/pquery.ml
+++ pplacer_src/pquery.ml
@@ -93,7 +93,9 @@ let make_ml_sorted = make Placement.ml_ratio
 let make_pp_sorted = make Placement.post_prob
 
 let uniform_namel namel = List.map (identity &&& const 1.) namel
-let set_mass pq m = { pq with namlom = [name pq, m] }
+let set_mass pq m =
+  let names = List.length pq.namlom |> float_of_int in
+  { pq with namlom = List.map (second (const (m /. names))) pq.namlom }
 let set_namlom pq nm = { pq with namlom = nm }
 
 let apply_to_place_list f pq =
