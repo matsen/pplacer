442e7534a333cf44f4d99203ff858bad614aaf26
Aaron Gallagher <habnabit@gmail.com>
2012-02-16 18:21:41 -0800

    Making gentree make trees or placefiles.

--- pplacer_src/gentree.ml
+++ pplacer_src/gentree.ml
@@ -4,12 +4,17 @@ let () =
   let a = float_of_string Sys.argv.(1)
   and b = float_of_string Sys.argv.(2)
   and size = int_of_string Sys.argv.(3)
-  and name = Sys.argv.(4)
-  and n_pqueries = int_of_string Sys.argv.(5)
   and r = Gsl_rng.make Gsl_rng.KNUTHRAN2002 in
   open_in_bin "/dev/random"
     |> with_dispose
         ~dispose:close_in
         (IO.read_i64 |- Int64.to_nativeint |- Gsl_rng.set r);
   let t = Commiesim.generate_lengthy_tree r ~a ~b size in
-  Commiesim.write_random_lengthy_pr r t name n_pqueries
+  if Array.length Sys.argv = 5 then
+    Newick_gtree.to_file t Sys.argv.(4)
+  else
+    let name_fmt = Scanf.format_from_string Sys.argv.(4) "%d"
+    and mkpr = Commiesim.write_random_lengthy_pr r t in
+    Array.sub Sys.argv 5 (Array.length Sys.argv - 5)
+      |> Array.map int_of_string
+      |> Array.iter (fun n -> mkpr (Printf.sprintf name_fmt n) n)
