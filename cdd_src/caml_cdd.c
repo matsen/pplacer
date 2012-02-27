fa9d4fbc1715c091f605668d539d04527bbb5766
Connor McCoy <cmccoy@fhcrc.org>
2012-02-27 13:59:49 -0800

    Spelling

--- cdd_src/caml_cdd.c
+++ cdd_src/caml_cdd.c
@@ -6,12 +6,12 @@
 
 double *extreme_vertices(const double *, const size_t, const size_t, size_t *);
 
-CAMLprim value caml_extreme_vertices(value verticies)
+CAMLprim value caml_extreme_vertices(value vertices)
 {
-  CAMLparam1(verticies);
-  double *vert_arr = Data_bigarray_val(verticies);
-  int nrows = (Bigarray_val(verticies)->dim[0]);
-  int ncols = (Bigarray_val(verticies)->dim[1]);
+  CAMLparam1(vertices);
+  double *vert_arr = Data_bigarray_val(vertices);
+  int nrows = (Bigarray_val(vertices)->dim[0]);
+  int ncols = (Bigarray_val(vertices)->dim[1]);
   size_t res_size;
   double *res = extreme_vertices(vert_arr, nrows, ncols, &res_size);
   value res_bigarr = alloc_bigarray_dims(BIGARRAY_FLOAT64 | BIGARRAY_C_LAYOUT, 2, res, res_size / 3, 3);
