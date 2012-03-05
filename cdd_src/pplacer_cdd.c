5c5e10077d9010e7ca3a685f44b49d6ec4fcb024
Connor McCoy <cmccoy@fhcrc.org>
2012-03-05 12:49:41 -0800

    Add upper bound to solution space; simplify
    This ended up changing the structure significantly: no longer a need to
    check for single-vertex solutions (the upper and lower bound ensure at
    least 2). Similarly, the process for extracting the inequality indices
    forming the hull becomes:
    
    For vertex v in  0..n_vertices-2, find & report inequality
    incident to both vertex v and vertex v+1. Since the upper bound isn't
    reported, the last inequality will be the one joining the final
    non-upper-bound vertex and the upper bound vertex.
    
    Also cleaned up some style

--- cdd_src/pplacer_cdd.c
+++ cdd_src/pplacer_cdd.c
@@ -12,18 +12,21 @@
 
 #ifdef PPLACER_CDD_TEST
 /* Debugging functions */
-static void dd_PrintMatrix(dd_Amatrix m, dd_rowrange rows, dd_colrange cols) {
+static void dd_PrintMatrix(dd_Amatrix m, dd_rowrange rows,
+                           dd_colrange cols)
+{
   dd_rowrange i;
   dd_colrange j;
-  for(i = 0; i < rows; i++) {
-    for(j = 0; j < cols; j++) {
-       printf("%e\t", *m[i][j]);
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < cols; j++) {
+      printf("%e\t", *m[i][j]);
     }
     printf("\n");
   }
 }
 
-void dd_PrintMatrixPtr(dd_MatrixPtr m) {
+void dd_PrintMatrixPtr(dd_MatrixPtr m)
+{
   dd_PrintMatrix(m->matrix, m->rowsize, m->colsize);
 }
 #endif
@@ -36,25 +39,34 @@ void dd_PrintMatrixPtr(dd_MatrixPtr m) {
  *
  * Caller must free returned matrix.
  */
-static dd_MatrixPtr init_ineq_doubles(const double* init_vals, int rows, int cols) {
+static dd_MatrixPtr init_ineq_doubles(const double *init_vals,
+                                      const size_t rows, const size_t cols,
+                                      const float lower_bound,
+                                      const float upper_bound)
+{
   /* Only 3-column inputs supported */
   assert(cols == 3);
 
-  dd_MatrixPtr m = dd_CreateMatrix(rows+1, cols);
+  dd_MatrixPtr m = dd_CreateMatrix(rows + 2, cols);
   dd_rowrange i;
   dd_colrange j;
 
   /* Fill values */
-  for(i = 0; i < m->rowsize-1; i++) {
-    for(j = 0; j < m->colsize; j++) {
-      dd_set_d(m->matrix[i][j], init_vals[m->colsize*i+j]);
+  for (i = 0; i < rows; i++) {
+    for (j = 0; j < m->colsize; j++) {
+      dd_set_d(m->matrix[i][j], init_vals[m->colsize * i + j]);
     }
   }
 
-  /* Add a row constraining solution space x>=0 */
-  dd_set_d(m->matrix[rows][0], 0); /* intercept */
-  dd_set_d(m->matrix[rows][1], 1); /* coef of x */
-  dd_set_d(m->matrix[rows][2], 0); /* coef of y */
+  /* Add a row constraining solution space x >= lower_bound */
+  dd_set_d(m->matrix[rows][0], lower_bound);    /* intercept */
+  dd_set_d(m->matrix[rows][1], 1);              /* coef of x */
+  dd_set_d(m->matrix[rows][2], 0);              /* coef of y */
+
+  /* Add a row constraining solution space x <= upper_bound */
+  dd_set_d(m->matrix[rows + 1][0], upper_bound);    /* intercept */
+  dd_set_d(m->matrix[rows + 1][1], -1);             /* coef of x */
+  dd_set_d(m->matrix[rows + 1][2], 0);              /* coef of y */
 
   /* Mark the representation as inequalities */
   m->representation = dd_Inequality;
@@ -64,18 +76,20 @@ static dd_MatrixPtr init_ineq_doubles(const double* init_vals, int rows, int col
   return m;
 }
 
-static int is_vertex(const mytype i) {
+static int is_vertex(const mytype i)
+{
   /* Vertices are 1, rays 0 */
   return *i > dd_almostzero;
 }
 
 /* Count the vertices in a matrix of external points */
-static size_t count_vertices(const dd_MatrixPtr generators) {
+static size_t count_vertices(const dd_MatrixPtr generators)
+{
   dd_rowrange i;
   int vertex_count = 0;
-  for(i = 0; i < generators->rowsize; i++) {
+  for (i = 0; i < generators->rowsize; i++) {
     /* Check if row is vertex (first column 0) */
-    if(is_vertex(generators->matrix[i][0]))
+    if (is_vertex(generators->matrix[i][0]))
       vertex_count++;
   }
 
@@ -83,15 +97,16 @@ static size_t count_vertices(const dd_MatrixPtr generators) {
 }
 
 /* Returns first item in s. s must be non-empty. */
-static long set_first(const set_type s) {
+static long set_first(const set_type s)
+{
   long elem;
 
-  for(elem = 1; elem <= s[0]; elem++) {
-    if(set_member(elem, s))  {
+  for (elem = 1; elem <= s[0]; elem++) {
+    if (set_member(elem, s)) {
       return elem;
     }
   }
-  assert(0); /* empty set */
+  assert(0);                    /* empty set */
 }
 
 /*
@@ -108,23 +123,28 @@ typedef struct {
  * Comparison function for qsort. Sorts by first column (desc), then second
  * (asc).
  */
-static int generator_row_cmp(const void *a, const void*b) {
-  const SortRow *ia = (const SortRow *)a;
-  const SortRow *ib = (const SortRow *)b;
+static int generator_row_cmp(const void *a, const void *b)
+{
+  const SortRow *ia = (const SortRow *) a;
+  const SortRow *ib = (const SortRow *) b;
 
   double da, db;
   da = *(ia->row[0]);
   db = *(ib->row[0]);
 
   /* Compare first column - sort descending */
-  if(db - da < 0.0) return -1; /* da > db */
-  if(db - da > 0.0) return 1;  /* da < db */
+  if (db - da < 0.0)
+    return -1;                  /* da > db */
+  if (db - da > 0.0)
+    return 1;                   /* da < db */
 
   /* Sort by second column - ascending */
   da = *(ia->row[1]);
   db = *(ib->row[1]);
-  if(da - db < 0.0) return -1;
-  if(da - db > 0.0) return 1;
+  if (da - db < 0.0)
+    return -1;
+  if (da - db > 0.0)
+    return 1;
 
   /* Tie */
   return 0;
@@ -136,11 +156,12 @@ static int generator_row_cmp(const void *a, const void*b) {
  * col 1 (asc)
  * This gives vertices before rays, ordered by point on x axis.
  */
-static size_t* sort_generators(dd_MatrixPtr m) {
+static size_t *sort_generators(dd_MatrixPtr m)
+{
   dd_rowrange i;
   /* Create SortRows for performing ordering */
-  SortRow* rows = (SortRow*)malloc(sizeof(SortRow)*m->rowsize);
-  for(i = 0; i < m->rowsize; i++) {
+  SortRow *rows = (SortRow *) malloc(sizeof(SortRow) * m->rowsize);
+  for (i = 0; i < m->rowsize; i++) {
     rows[i].index = i;
     rows[i].row = m->matrix[i];
   }
@@ -149,8 +170,8 @@ static size_t* sort_generators(dd_MatrixPtr m) {
   qsort(rows, m->rowsize, sizeof(SortRow), generator_row_cmp);
 
   /* Extract sorted indices */
-  size_t* result = (size_t*)malloc(sizeof(size_t)*m->rowsize);
-  for(i = 0; i < m->rowsize; i++)
+  size_t *result = (size_t *) malloc(sizeof(size_t) * m->rowsize);
+  for (i = 0; i < m->rowsize; i++)
     result[i] = rows[i].index;
 
   free(rows);
@@ -167,115 +188,72 @@ static size_t* sort_generators(dd_MatrixPtr m) {
  * the polytope to the right of the vertex. output_size is set to the total
  * length of the ouptut.
  */
-static double* list_extreme_vertices(const dd_MatrixPtr generators,
-    const dd_SetFamilyPtr incidence, const size_t nrows, long added_index,
-    /*OUT*/size_t* output_size) {
+static double *list_extreme_vertices(const dd_MatrixPtr generators,
+                                     const dd_SetFamilyPtr incidence,
+                                     const size_t nrows,
+                                     long lower_bound_index,
+                                     /*OUT*/ size_t * output_size)
+{
   assert(generators->rowsize > 0);
   assert(generators->colsize > 2);
 
-  /* At most, all nrows are included in the hull */
-  set_type cur_vert_set, next_vert_set, prev_vert_set, s;
+  set_type cur_vert_set, next_vert_set, s;
   dd_rowrange i;
   long elem;
   size_t out_row = 0, r;
 
-  /*
-   * Determine output size by counting vertices (and omitting rays)
-   *
-   * If The polytope has only one vertex, and that vertex is the origin, no
-   * vertices are returned - only rays.
-   */
   size_t vertex_count = count_vertices(generators);
-  *output_size = !vertex_count ? 3 : 3*vertex_count;
-  double* output = (double*)malloc(sizeof(double)*(*output_size));
-  size_t* indices = sort_generators(generators);
+
+  /* Last vertex intersects with upper bound - not output */
+  *output_size = 3 * (vertex_count - 1);
+  double *output = (double *) malloc(sizeof(double) * (*output_size));
+
+  /* Sorted indices into generators */
+  size_t *indices = sort_generators(generators);
 
   assert(*output_size > 0);
 
-  /*
-   * One special case:
-   * cddlib doesn't report solution vertices containing only the origin.
-   * Solutions with the origin and additional points are reported.
+  /* Loop over vertices in generators, extracting solutions
+   *
+   * For vertices 0..n_vertices-2, find the inequality incident to the vertex
+   * also incident in the next vertex.
    */
-  if(!vertex_count) { /* Origin-only solution = no vertices */
-    assert(generators->rowsize == 2);
-    /* Find the first ray without added_index incident - this is the ray to the
-       right of the origin. */
-    for(i = 0; i < generators->rowsize; i++) {
-      r = indices[i];
-      cur_vert_set = incidence->set[r];
+  for (i = 0; i < vertex_count - 1; i++) {
+    r = indices[i];
 
-      if(set_member(added_index, cur_vert_set)) /* Ray has added_index incident */
-        continue;
+    assert(is_vertex(generators->matrix[r][0]));
+    assert(is_vertex(generators->matrix[indices[i + 1]][0]));
 
-      assert(set_card(cur_vert_set) == 1);
+    assert(out_row < vertex_count - 1);
+    cur_vert_set = incidence->set[r];
 
-      set_initialize(&s, cur_vert_set[0]);
-      set_copy(s, cur_vert_set);
-      set_delelem(s, added_index);
-      assert(set_card(s) == 1);
+    next_vert_set = incidence->set[indices[i + 1]];
 
-      output[0] = 0.0;                        /* x */
-      output[1] = 0.0;                        /* y */
-      output[2] = (double)(set_first(s) - 1); /* inequality index */
-      set_free(s);
-      break;
-    }
-  } else { /* 1+ vertices */
-    for(i = 0; i < generators->rowsize; i++) {
-      r = indices[i];
-      if(is_vertex(generators->matrix[r][0])) {
-        assert(out_row < vertex_count);
-        cur_vert_set = incidence->set[r];
+    /* Sets should be same size */
 
-        if(vertex_count == 1 && i == 0) {
-          /* First and only vertex. Result is just: */
-          /*    current_set \ {added_index} */
-          /* added_index is removed below. */
-          set_initialize(&s, cur_vert_set[0]);
-          set_copy(s, cur_vert_set);
-        } else if(i < vertex_count - 1) { /* Not the last vertex */
-          next_vert_set = incidence->set[indices[i+1]];
+    assert(cur_vert_set[0] == next_vert_set[0]);
+    set_initialize(&s, cur_vert_set[0]);
+    set_int(s, cur_vert_set, next_vert_set);
 
-          /* Sets should be same size */
-          assert(cur_vert_set[0] == next_vert_set[0]);
-          set_initialize(&s, cur_vert_set[0]);
-          set_int(s, cur_vert_set, next_vert_set);
-        } else { /* Last vertex */
-          /* Previous set instead of next */
-          assert(i);
-          prev_vert_set = incidence->set[indices[i-1]];
+    /* Remove added index for first item */
+    /*if (!i)*/
+      /*set_delelem(s, lower_bound_index);*/
 
-          /* Sets should be same size */
-          assert(cur_vert_set[0] == prev_vert_set[0]);
-          set_initialize(&s, cur_vert_set[0]);
+    /* Set may have > 1 solution if ~identical slopes and intercepts due to
+     * rounding. */
+    /*assert(set_card(s) == 1); */
 
-          /* Diff, instead of intersection */
-          set_diff(s, cur_vert_set, prev_vert_set);
-        }
+    /* Only one item in the set */
+    elem = set_first(s);
+    set_free(s);
 
-        /* Remove added index for first item */
-        if(!i) set_delelem(s, added_index);
+    /* Fill output row */
+    int base = out_row * 3;
+    output[base] = *generators->matrix[r][1];     /* x */
+    output[base + 1] = *generators->matrix[r][2]; /* y */
+    output[base + 2] = (double) (elem - 1);       /* ineq index, converted to 0-base */
 
-        /* Set may have > 1 solution if ~identical slopes and intercepts due to
-         * rounding. */
-        /*assert(set_card(s) == 1);*/
-
-        /* Only one item in the set */
-        elem = set_first(s);
-        set_free(s);
-
-        /* Fill output row */
-        int base = out_row * 3;
-        output[base] = *generators->matrix[r][1];   /* x */
-        output[base+1] = *generators->matrix[r][2]; /* y */
-        output[base+2] = (double)(elem - 1);        /* ineq index, converted to 0-base */
-
-        out_row++;
-      } else {
-        assert(out_row == vertex_count);
-      }
-    }
+    out_row++;
   }
 
   free(indices);
@@ -285,9 +263,15 @@ static double* list_extreme_vertices(const dd_MatrixPtr generators,
 
 
 /*
- * Find the extreme vertices of the convex hull of ineqs.
+ * Find the extreme vertices between lower_bound and upper_bound of the convex
+ * hull of ineqs.
  *
- * ineqs: row-major order matrix of total length row*cols
+ * ineqs: row-major order matrix of total length nrow*ncols
+ * nrows: Number of rows in matrix
+ * ncols: Number of columns in matrix
+ * lower_bound: lower bound of solution space on x-axis
+ * upper_bound: upper bound of solution space on y-axis
+ * output size: Set to size of result
  *
  * Returns array representing 3 by (output_size/3) matrix, where each row is:
  *   x_i, y_i, l_i
@@ -295,13 +279,16 @@ static double* list_extreme_vertices(const dd_MatrixPtr generators,
  * the input inequality that constrains the solution space *to the right* of
  * vertex i.
  */
-double* extreme_vertices(const double* ineqs, const size_t nrows, const size_t cols,
-    /*OUT*/ size_t* output_size) {
+double *extreme_vertices(const double *ineqs, const size_t nrows,
+                         const size_t ncols, float lower_bound,
+                         float upper_bound, /*OUT*/ size_t * output_size)
+{
 
   /* Preconditions */
   assert(ineqs != NULL);
-  assert(cols == 3);
+  assert(ncols == 3);
   assert(output_size != NULL);
+  assert(lower_bound <= upper_bound);
 
   /*
    * Initialize library
@@ -310,12 +297,13 @@ double* extreme_vertices(const double* ineqs, const size_t nrows, const size_t c
   dd_set_global_constants();
   dd_ErrorType err;
   dd_MatrixPtr generators;
-  dd_MatrixPtr m = init_ineq_doubles(ineqs, nrows, cols);
+  dd_MatrixPtr m =
+      init_ineq_doubles(ineqs, nrows, ncols, lower_bound, upper_bound);
   dd_SetFamilyPtr incidence;
 
   /* Outputs */
   dd_PolyhedraPtr poly = dd_DDMatrix2Poly(m, &err);
-  if(err != dd_NoError) {
+  if (err != dd_NoError) {
     return NULL;
   }
 
@@ -325,7 +313,9 @@ double* extreme_vertices(const double* ineqs, const size_t nrows, const size_t c
   /* Get incidence */
   incidence = dd_CopyIncidence(poly);
 
-  double* result = list_extreme_vertices(generators, incidence, nrows, m->rowsize, output_size);
+  double *result =
+      list_extreme_vertices(generators, incidence, nrows, m->rowsize - 1,
+          output_size);
 
   dd_FreeMatrix(m);
   dd_FreeMatrix(generators);
@@ -341,37 +331,41 @@ double* extreme_vertices(const double* ineqs, const size_t nrows, const size_t c
 /*
  * Helper function to generate a matrix from init_vals
  */
-static dd_MatrixPtr init_matrix(const double* init_vals, int rows, int cols) {
+static dd_MatrixPtr init_matrix(const double *init_vals, int rows,
+                                int cols)
+{
   dd_MatrixPtr m = dd_CreateMatrix(rows, cols);
   dd_rowrange i;
   dd_colrange j;
 
   /* Fill values */
-  for(i = 0; i < m->rowsize; i++) {
-    for(j = 0; j < m->colsize; j++) {
-      dd_set_d(m->matrix[i][j], init_vals[m->colsize*i+j]);
+  for (i = 0; i < m->rowsize; i++) {
+    for (j = 0; j < m->colsize; j++) {
+      dd_set_d(m->matrix[i][j], init_vals[m->colsize * i + j]);
     }
   }
   return m;
 }
 
-static void test_sort_generators() {
+static void test_sort_generators()
+{
   const double input[] = {
     1.0, 1.0, 2.0,
     1.0, 0.0, 0.0,
     0.0, 0.0, -1.0,
     0.0, 0.2, 1.0,
-    1.0, 2.0, 3.0 };
+    1.0, 2.0, 3.0
+  };
 
   const int rows = 5;
   const int cols = 3;
 
   dd_MatrixPtr m = init_matrix(input, rows, cols);
-  size_t* sorted_indices = sort_generators(m);
+  size_t *sorted_indices = sort_generators(m);
   dd_FreeMatrix(m);
-  size_t expected[] = {1, 0, 4, 2, 3};
+  size_t expected[] = { 1, 0, 4, 2, 3 };
   int i;
-  for(i = 0; i < rows; i++) {
+  for (i = 0; i < rows; i++) {
     assert(sorted_indices[i] == expected[i]);
   }
 
@@ -380,21 +374,24 @@ static void test_sort_generators() {
 
 /* Test data */
 const double INPUT_MATRIX[] = {
-  0.0, 2.0, -1.0 ,
-  1.0, 1.0, -1.0 ,
-  2.0, 0.5, -1.0 ,
+  0.0, 2.0, -1.0,
+  1.0, 1.0, -1.0,
+  2.0, 0.5, -1.0,
   15.0, 1.0, -1.0,
   16.0, 1.0, -1.0,
-  17.0, 1.0, -1.0 };
+  17.0, 1.0, -1.0
+};
 
 const int ROWS = 6;
 const int COLS = 3;
 
 /* Run a simple test */
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   /* Init cdd */
   size_t result_size;
-  double* result = extreme_vertices(INPUT_MATRIX, ROWS, COLS, &result_size);
+  double *result =
+      extreme_vertices(INPUT_MATRIX, ROWS, COLS, 0.0, 10.0, &result_size);
   assert(result_size == 9);
 
   assert(abs(result[0]) < EPS);
@@ -412,6 +409,8 @@ int main(int argc, char* argv[]) {
   test_sort_generators();
 
   free(result);
+
+  printf("OK\n");
   return 0;
 }
 #endif
