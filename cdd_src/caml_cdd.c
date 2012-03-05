e86e8addc6179f10597aa1ff48052debb540d6ba
Connor McCoy <cmccoy@fhcrc.org>
2012-03-05 13:33:22 -0800

    Add lower and upper bound to caml_cdd
    Supplying bounds from OCaml needs to be done.

--- cdd_src/caml_cdd.c
+++ cdd_src/caml_cdd.c
@@ -5,16 +5,16 @@
 #include <caml/alloc.h>
 #include <caml/custom.h>
 
-double *extreme_vertices(const double *, const size_t, const size_t, size_t *);
+double *extreme_vertices(const double *, const size_t, const size_t, float, float, size_t *);
 
-CAMLprim value caml_extreme_vertices(value vertices)
+CAMLprim value caml_extreme_vertices(value vertices, value lower_bound, value upper_bound)
 {
-  CAMLparam1(vertices);
+  CAMLparam3(vertices, lower_bound, upper_bound);
   double *vert_arr = Data_bigarray_val(vertices);
   int nrows = (Bigarray_val(vertices)->dim[0]);
   int ncols = (Bigarray_val(vertices)->dim[1]);
   size_t res_size;
-  double *res = extreme_vertices(vert_arr, nrows, ncols, &res_size);
+  double *res = extreme_vertices(vert_arr, nrows, ncols, Double_val(lower_bound), Double_val(upper_bound), &res_size);
   value res_bigarr = alloc_bigarray_dims(BIGARRAY_FLOAT64 | BIGARRAY_C_LAYOUT, 2, NULL, res_size / 3, 3);
   memcpy(Data_bigarray_val(res_bigarr), res, sizeof *res * res_size);
   free(res);
--- cdd_src/cdd.ml
+++ cdd_src/cdd.ml
@@ -2,7 +2,7 @@ module BA = Bigarray
 module BA2 = BA.Array2
 
 type float_ba2 = (float, BA.float64_elt, BA.c_layout) BA2.t
-external c_extreme_vertices: float_ba2 -> float_ba2 = "caml_extreme_vertices"
+external c_extreme_vertices: float_ba2 -> float -> float -> float_ba2 = "caml_extreme_vertices"
 
 let extreme_vertices parr =
   let ba = BA2.create BA.float64 BA.c_layout (Array.length parr) 3 in
@@ -10,7 +10,8 @@ let extreme_vertices parr =
   Array.iteri
     (fun i (a, b) -> BA2.unsafe_set ba i 0 a; BA2.unsafe_set ba i 1 b)
     parr;
-  let res = c_extreme_vertices ba in
+  (* Aaron: bounds need to be added here *)
+  let res = c_extreme_vertices ba 0.0 10.0 in
   Array.init
     (BA2.dim1 res)
     (fun i ->
