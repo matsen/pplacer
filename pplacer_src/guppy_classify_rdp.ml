584a996c65310e28582bac3374286d16a42e1450
Aaron Gallagher <habnabit@gmail.com>
2012-03-16 14:27:45 -0700

    Updating `guppy classify_rdp`.

--- pplacer_src/guppy_classify_rdp.ml
+++ pplacer_src/guppy_classify_rdp.ml
@@ -7,16 +7,14 @@ object (self)
   inherit subcommand () as super
   inherit refpkg_cmd ~required:true as super_refpkg
   inherit sqlite_cmd () as super_sqlite
-  inherit output_cmd ~prefix_required:true () as super_output
 
-  val csv_out = flag "--csv"
-    (Plain (false, "Write .class.csv files containing CSV data."))
+  val cutoff = flag "--cutoff"
+    (Formatted (0.8, "The bootstrap cutoff for which classifications are considered best. default: %g"))
 
   method specl =
-     toggle_flag csv_out
+     float_flag cutoff
   :: super_refpkg#specl
    @ super_sqlite#specl
-   @ super_output#specl
 
   method desc = "converts RDP output to something resmbling guppy classify output"
   method usage = "usage: classify_rdp -c some.refpkg rdp_output_file[s]"
@@ -27,62 +25,56 @@ object (self)
     let name_map = Tax_id.TaxIdMap.enum td.Tax_taxonomy.tax_name_map
       |> Enum.map (curry identity |> flip |> uncurry |- second Tax_id.to_string)
       |> StringMap.of_enum
-    and sqlite_out = match !(sqlite_fname.Subcommand.value) with
-      | Some _ -> true
-      | None -> false
+    and cutoff = fv cutoff in
+
+    let db = self#get_db in
+    Sql.check_exec db "BEGIN TRANSACTION";
+
+    let st = Sqlite3.prepare db
+      "INSERT INTO runs VALUES (NULL, ?)"
+    in
+    Sql.bind_step_reset db st
+      [|Sql.D.TEXT (Array.to_list Sys.argv |> String.concat " ")|];
+    let run_id = Sqlite3.last_insert_rowid db in
+    let new_place_id =
+      let p_st = Sqlite3.prepare db
+        "INSERT INTO placements VALUES (NULL, ?, ?)"
+      in
+      fun classifier ->
+        Sql.bind_step_reset db p_st
+          [|Sql.D.TEXT classifier; Sql.D.INT run_id|];
+        Sqlite3.last_insert_rowid db
     in
 
-    let open_out name =
-      (self#single_prefix ()) ^ (Filename.basename name) |> open_out
+    let pn_st = Sqlite3.prepare db
+      "INSERT INTO placement_names VALUES (?, ?, ?, 1.);"
+    and pc_st = Sqlite3.prepare db
+      "INSERT INTO placement_nbc VALUES (?, ?, ?, ?, ?)"
+    and mc_st = Sqlite3.prepare db
+      "INSERT INTO multiclass VALUES (?, ?, ?, ?, ?, ?)"
     in
 
-    let out_func =
-      if sqlite_out then begin
-        let db = self#get_db in
-        Sql.check_exec db "BEGIN TRANSACTION";
-        let pn_st = Sqlite3.prepare db
-          "INSERT INTO placement_names VALUES (?, ?, ?, 1.);"
-        and pc_st = Sqlite3.prepare db
-          "INSERT INTO placement_classifications VALUES (?, ?, ?, ?, ?)"
-        in
-        let close () =
-          Sql.check_exec db "COMMIT";
-          Sql.close db
-        and process origin name =
-          Sql.check_exec db "INSERT INTO placements VALUES (NULL)";
-          let place_id = Sqlite3.last_insert_rowid db in
-          Sql.bind_step_reset db pn_st
-            [|
-              Sql.D.INT place_id;
-              Sql.D.TEXT name;
-              Sql.D.TEXT origin;
-            |];
-          List.iter
-            (Array.map (fun x -> Sql.D.TEXT x)
-             |- Array.append [| Sql.D.INT place_id |]
-             |- Sql.bind_step_reset db pc_st)
-
-        in
-        finally close process
-
-      end else if fv csv_out then begin fun origin ->
-        let ch = open_out origin in
-        let csvch = csv_out_channel ch |> Csv.to_out_obj in
-        Csv.output_record
-          csvch
-          ["name"; "origin"; "desired_rank"; "rank"; "tax_id"; "likelihood"];
-        fun name -> List.map (Array.append [| name; origin |] |- Array.to_list)
-          |- Csv.output_all csvch
-          |> finally (fun () -> close_out ch)
-
-      end else begin fun origin ->
-        let ch = open_out origin in
-        fun name -> List.map (Array.append [| name; origin |])
-          |- Array.of_list
-          |- String_matrix.write_padded ch
-          |> finally (fun () -> close_out ch)
-
-      end
+    let process origin name rows =
+      let place_id = new_place_id "rdp" in
+      Sql.bind_step_reset db pn_st
+        [|
+          Sql.D.INT place_id;
+          Sql.D.TEXT name;
+          Sql.D.TEXT origin;
+        |];
+      List.iter
+        (Array.map (fun x -> Sql.D.TEXT x)
+            |- Array.append [| Sql.D.INT place_id |]
+            |- Sql.bind_step_reset db pc_st)
+        rows;
+      List.iter
+        (junction
+           (fun arr -> float_of_string arr.(3) < cutoff)
+           (const ())
+           (Array.map (fun x -> Sql.D.TEXT x)
+            |- Array.append [| Sql.D.INT place_id; Sql.D.TEXT name |]
+            |- Sql.bind_step_reset db mc_st))
+        rows;
 
     and classify line =
       (* past participle of 'to split' *)
@@ -105,6 +97,9 @@ object (self)
     List.enum argl
       |> Enum.map
           (identity &&& (File.lines_of |- Enum.map classify))
-      |> Enum.iter (fun (a, bcl) -> Enum.iter (out_func a |> uncurry) bcl)
+      |> Enum.iter (fun (a, bcl) -> Enum.iter (process a |> uncurry) bcl);
+
+    Sql.check_exec db "COMMIT";
+    Sql.close db
 
 end
