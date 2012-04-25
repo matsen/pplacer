4b73344b75a9cac410fa699dfe9005404945bda7
Aaron Gallagher <habnabit@gmail.com>
2012-04-25 13:22:30 -0700

    Improvements on hrefpkg_query.py.
     * Adding an actual output for the classification database.
     * Adding logging messages.
     * Snarfing the rank of the initial classification out of the index refpkg's
       metadata instead of passing it as a flag.

--- scripts/hrefpkg_query.py
+++ scripts/hrefpkg_query.py
@@ -1,32 +1,43 @@
 import subprocess
 import argparse
 import tempfile
+import logging
 import os.path
 import sqlite3
+import shutil
 
+from taxtastic.refpkg import Refpkg
 from Bio import SeqIO
 
+log = logging.getLogger(__name__)
+
 def main():
+    logging.basicConfig(
+        level=logging.INFO, format="%(levelname)s: %(message)s")
+
     parser = argparse.ArgumentParser()
     parser.add_argument('hrefpkg')
     parser.add_argument('query_seqs')
+    parser.add_argument('classification_db')
     parser.add_argument('--pplacer', default='pplacer')
     parser.add_argument('--guppy', default='guppy')
     parser.add_argument('--rppr', default='rppr')
     parser.add_argument('--refpkg-align', default='refpkg_align.py')
-    parser.add_argument('--index-rank', default='family')
 
     args = parser.parse_args()
     workdir = tempfile.mkdtemp()
 
     classif_db = os.path.join(workdir, 'classifications.sqlite')
     index_refpkg = os.path.join(args.hrefpkg, 'index.refpkg')
+    index = Refpkg(index_refpkg)
+    index_rank = index.metadata('index_rank')
+    log.info('performing initial classification at %s', index_rank)
     subprocess.check_call(
         [args.rppr, 'prep_db', '--sqlite', classif_db, '-c', index_refpkg])
     subprocess.check_call(
         [args.guppy, 'classify', '--sqlite', classif_db, '-c', index_refpkg,
-         '--classifier', 'nbc', '--nbc-rank', args.index_rank, '--no-pre-mask',
-         '--nbc-sequences', args.query_seqs])
+         '--classifier', 'nbc', '--nbc-rank', index_rank,
+         '--no-pre-mask', '--nbc-sequences', args.query_seqs])
 
     conn = sqlite3.connect(classif_db)
     curs = conn.cursor()
@@ -36,9 +47,11 @@ def main():
         FROM   multiclass
         WHERE  want_rank = ?
                AND rank = want_rank
-    """, (args.index_rank,))
+    """, (index_rank,))
     seq_bins = dict(curs)
     all_bins = set(seq_bins.itervalues())
+    log.info('classified into %d bins', len(all_bins))
+    log.debug('bins: %r', all_bins)
     bin_outputs = {}
     for bin in all_bins:
         bin_outputs[bin] = open(os.path.join(workdir, bin + '.fasta'), 'w')
@@ -52,7 +65,8 @@ def main():
     for fobj in bin_outputs.itervalues():
         fobj.close()
 
-    for bin in all_bins:
+    for e, bin in enumerate(all_bins):
+        log.info('classifying bin %s (%d/%d)', bin, e + 1, len(all_bins))
         unaligned = os.path.join(workdir, bin + '.fasta')
         aligned = os.path.join(workdir, bin + '-aligned.sto')
         placed = os.path.join(workdir, bin + '.jplace')
@@ -65,6 +79,6 @@ def main():
         subprocess.check_call(
             [args.guppy, 'classify', '--sqlite', classif_db, '-c', refpkg, placed])
 
-    print workdir
+    shutil.copy(classif_db, args.classification_db)
 
 main()
