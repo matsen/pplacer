2207971a06c2f9f9729a9cdc62bb8879fae6b72f
Erick Matsen <matsen@fhcrc.org>
2012-05-21 16:05:46 -0700

    a full sketch of indep_contrasts

--- pplacer_src/indep_contrasts.ml
+++ pplacer_src/indep_contrasts.ml
@@ -36,3 +36,28 @@ let binary_opt_extend f oa ob = match (oa, ob) with
 let combo = binary_opt_extend actual_combo
 let sum = binary_opt_extend actual_sum
 
+(* px is a pair, as above, and bx is a branch length.*)
+let join_two_subsolns pi bi pj bj =
+  combo (sum pi (0., bi)) (sum pj (0., bj))
+
+(* l is a list of pairs (p1, b1), ..., (pk, bk) *)
+let join_subsoln_list l =
+  (* this is just a fold *)
+
+(* So, to get all of the distal p's, just do a DFS recursion across the tree.
+ * The base case for something that is not labeled with a character (e.g. a copy
+ * number) is None, whereas if there is something x given then the base case is
+ * Some (x,0). At every internal node, do a join_subsoln_list.
+ *)
+let build_distal_map t leaf_values =
+  (* build a map of all internal nodes to the corresponding p's *)
+
+(* Now build a proximal_map using this proximal map.
+ * This will be a recursion as follows: say we are at an internal node such that
+ * pd is the distal pair coming from the previous step of the recursion, and bd
+ * is the branch length connecting this internal node to the proximal part of
+ * the tree.
+ * Now, say (p1, b1), ..., (pk, bk) are below us. proximal p for edge i will be
+ * join_subsoln_list [p1, b1; ...; p(i-1), b(i-1); p(i+1), b(i+1); ...; pk, bk; pd, bd]
+ *)
+let build_proximal_map t distal_map =
