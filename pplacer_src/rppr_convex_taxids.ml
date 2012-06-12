ccde62baa383e924bdaccdace8a69188347e77f0
Aaron Gallagher <habnabit@gmail.com>
2012-06-12 16:59:22 -0700

    Refactoring/fixing `rppr convex_taxids`; adding tests.

--- pplacer_src/rppr_convex_taxids.ml
+++ pplacer_src/rppr_convex_taxids.ml
@@ -3,6 +3,33 @@ open Guppy_cmdobjs
 open Ppatteries
 open Convex
 
+let of_refpkg rp =
+  let gt = Refpkg.get_ref_tree rp
+  and td = Refpkg.get_taxonomy rp in
+  let st = Gtree.get_stree gt
+  and top_id = Gtree.top_id gt in
+  rank_tax_map_of_refpkg rp
+    |> IntMap.enum
+    |> Enum.map
+        (fun (rank, taxmap) ->
+          let sizemim, cutsetim = build_sizemim_and_cutsetim (taxmap, st) in
+          let cutsetim = IntMap.add (Stree.top_id st) ColorSet.empty cutsetim in
+          let unconvex_colors = IntMap.fold
+            (fun _ colors unconvex ->
+              if ColorSet.cardinal colors < 2 then unconvex else
+                ColorSet.union unconvex colors)
+            cutsetim
+            ColorSet.empty
+          and all_colors = IntMap.find top_id sizemim
+            |> ColorMap.keys
+            |> ColorSet.of_enum
+          and rank_name = Tax_taxonomy.get_rank_name td rank in
+          ColorSet.diff all_colors unconvex_colors
+            |> ColorSet.enum
+            |> Enum.map (fun c -> [rank_name; Tax_id.to_string c]))
+    |> Enum.flatten
+
+
 class cmd () =
 object (self)
   inherit subcommand () as super
@@ -17,28 +44,7 @@ object (self)
   method usage = "usage: convex_taxids -c my.refpkg"
 
   method action _ =
-    let rp = self#get_rp in
-    let gt = Refpkg.get_ref_tree rp
-    and td = Refpkg.get_taxonomy rp in
-    let st = gt.Gtree.stree in
-    rank_tax_map_of_refpkg rp
-      |> IntMap.enum
-      |> Enum.map
-          (fun (rank, taxmap) ->
-            let _, cutsetim = build_sizemim_and_cutsetim (taxmap, st) in
-            let cutsetim = IntMap.add (Stree.top_id st) ColorSet.empty cutsetim in
-            let all_colors, unconvex_colors = IntMap.fold
-              (fun _ colors (all, unconvex) ->
-                ColorSet.union all colors,
-                if ColorSet.cardinal colors < 2 then unconvex else
-                  ColorSet.union unconvex colors)
-              cutsetim
-              (ColorSet.empty, ColorSet.empty)
-            and rank_name = Tax_taxonomy.get_rank_name td rank in
-            ColorSet.diff all_colors unconvex_colors
-              |> ColorSet.enum
-              |> Enum.map (fun c -> [rank_name; Tax_id.to_string c]))
-      |> Enum.flatten
+    of_refpkg self#get_rp
       |> List.of_enum
       |> List.cons ["rank"; "tax_id"]
       |> self#write_ll_tab
--- tests/rppr/test_all_rppr.ml
+++ tests/rppr/test_all_rppr.ml
@@ -2,4 +2,5 @@ open OUnit
 
 let suite = [
   "voronoi" >::: Test_voronoi.suite;
+  "convex_taxids" >::: Test_convex_taxids.suite;
 ]
