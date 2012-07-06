0b6a88efefad7e61bf5ac468ea76637143edd91f
Aaron Gallagher <habnabit@gmail.com>
2012-07-06 14:53:59 -0700

    Adding tests for quadratic entropy; --variance.
    Also, since quadratic entropy can't be calculated with non-uniform mass, omit
    it in that case. Passing --variance in the same case raises an error since it
    was explicitly requested and won't produce sensible results.

--- tests/guppy/test_rarefact.ml
+++ tests/guppy/test_rarefact.ml
@@ -9,33 +9,40 @@ let check_triples =
          a1 b1 c1 a2 b2 c2)
       @? (a1 = a2 && b1 =~ b2 && c1 =~ c2))
 
+let check_quipples =
+  Enum.iter2
+    (fun (a1, b1, c1, d1) (a2, b2, c2, d2) ->
+      (Printf.sprintf "unequal (%d(%g, %g, %g) and %d(%g, %g, %g))"
+         a1 b1 c1 d1 a2 b2 c2 d2)
+      @? (a1 = a2 && b1 =~ b2 && c1 =~ c2 && d1 =~ d2))
+
 let suite = [
   "test_simple" >:: begin fun () ->
     placerun_of_dir "simple" "test1"
       |> Rarefaction.of_placerun Placement.ml_ratio
-      |> check_triples
+      |> check_quipples
           (List.enum [
-            2, 8., 15.;
+            2, 8., 15., 2.;
           ])
   end;
 
   "test_multi" >:: begin fun () ->
     placerun_of_dir "multi" "test1and3"
       |> Rarefaction.of_placerun Placement.ml_ratio
-      |> check_triples
+      |> check_quipples
           (List.enum [
-            2, 6.66667, 12.33333;
-            3, 10., 15.;
+            2, 6.66667, 12.33333, 1.66667;
+            3, 10., 15., 2.22222;
           ])
   end;
 
   "test_hand_mean" >:: begin fun () ->
     placerun_of_dir "misc" "test_rarefaction"
       |> Rarefaction.of_placerun Placement.ml_ratio
-      |> check_triples
+      |> check_quipples
           (List.enum [
-            2, 4.66667, 5.;
-            3, 7., 7.;
+            2, 4.66667, 5., 1.16667;
+            3, 7., 7., 1.55556;
           ])
   end;
 
