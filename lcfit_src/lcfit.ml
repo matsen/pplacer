6f6c3d728c5f8ceaefeb1fd4165f9f41b3d7775e
Connor McCoy <cmccoy@fhcrc.org>
2013-01-23 11:27:12 -0800

    A stab at calc_marg_prob
    Currently duplicates a lot of code in three_tax.ml

--- lcfit_src/lcfit.ml
+++ lcfit_src/lcfit.ml
@@ -1,9 +1,12 @@
 (* OCaml-C interface to lcfit_tripod *)
 
+open Ppatteries
+
 module BA = Bigarray
 module BA1 = Bigarray.Array1
 
-type float_vector = (float, BA.float64_elt, BA.c_layout) BA1.t
+(* distal branch length, pendant branch length, log-likelihood *)
+type tripod_point = (float * float * float)
 
 (* The binary symmetric model for a tripod, with one branch length fixed *)
 type tripod_bsm = {n00: float; n01: float; n10: float; n11: float;
@@ -18,19 +21,91 @@ external jacobian: tripod_bsm -> float -> float -> float array = "caml_lcfit_tri
 
 (* Fit the BSM given vectors of (dist_bl, pend_bl, log_like) and an initial estimate of the
    model *)
-external fit: tripod_bsm -> (float * float * float) array -> tripod_bsm = "caml_lcfit_tripod_fit"
+external fit: tripod_bsm -> tripod_point array -> tripod_bsm = "caml_lcfit_tripod_fit"
+
+(* Heap ordered by log-likelihood *)
+module PointHeap = Heap.Make(struct
+  type t = tripod_point
+  let compare (_,_,a) (_,_,b) = Float.compare a b
+end)
 
 (* Rescale m to intersect with (dist_bl, pend_bl, ll) *)
 let rescale (dist_bl, pend_bl, ll) m =
   let est_ll = log_like m dist_bl pend_bl in
   let fac = ll /. est_ll in
   {m with n00=m.n00 *. fac;
-    n01=m.n01 *. fac;
-    n10=m.n10 *. fac;
-    n11=m.n11 *. fac}
+          n01=m.n01 *. fac;
+          n10=m.n10 *. fac;
+          n11=m.n11 *. fac}
 
+(* This seems to provide sensible defaults *)
+let default_model t =
+  {n00=1500.;n01=300.;n10=300.;n11=300.;r=1.;b=0.5;t=t;rx=1.;bx=0.5}
 
-let run_test() =
-  let test_model = {n00=1500.;n01=300.;n10=300.;n11=300.;r=1.;b=0.5;t=0.390296;rx=1.;bx=0.5} in
-  let scaled = rescale (0.1, 0.1, -4000.0) test_model in
-  Printf.printf "%f -> %f\n" test_model.n00 scaled.n00
+(* Evaluate log_like at the cartesian product of points sampled uniformly
+ * between [0,cut_bl] and [0,max_pend], fit the tripod_bsm using `keep_top` of
+ * these points *)
+let find_pts_fit_model ?(n_dist=5) ?(n_pend=5) ?(keep_top=20) cut_bl max_pend prior log_like =
+  let choose_pts n f = 1 -- n
+    |> map Float.of_int
+    |> map f
+    |> List.of_enum
+  in
+  let dist_bls = choose_pts n_dist (fun i -> i /. cut_bl)
+  and pend_bls = choose_pts n_pend (fun i -> i /. max_pend) 
+  in
+  let pt_heap = List.cartesian_product dist_bls pend_bls
+    |> List.map (fun (dist_bl, pend_bl) ->
+        (dist_bl, pend_bl, (log_like dist_bl pend_bl)))
+    |> PointHeap.of_list
+  in
+  let pts = pt_heap
+    |> PointHeap.enum
+    |> Enum.take keep_top
+    |> List.of_enum
+  in
+  let max_ll = PointHeap.enum pt_heap |> Enum.get |> Option.get in
+  let init_model = default_model cut_bl |> rescale max_ll in
+  fit init_model (Array.of_list pts)
+
+let calc_marg_prob model max_pend cut_bl prior base_ll upper_limit =
+  (* Select some points to sample - uniformly from 0-cut_bl, 0-max_pend *)
+  let max_n_exceptions = 10
+  and n_exceptions = ref 0
+  and ll = log_like model
+  and abs_err = 0.
+  and rel_err = 1e-2
+  in
+  let rec perform upper_limit =
+    if !n_exceptions >= max_n_exceptions then begin
+      Printf.printf
+      "Warning: integration did not converge after changing bounds %d times\n"
+      max_n_exceptions;
+      0.
+    end
+    else
+      let inner_integration dist_bl =
+        Integration.value_integrate
+          (fun pend_bl -> 
+            (exp ((ll dist_bl pend_bl) -. base_ll)) *. (prior pend_bl)
+            ll dist_bl pend_bl)
+          0. upper_limit ~abs_err ~rel_err
+      in
+      let outer_integration () =
+        (/.)
+          (Integration.value_integrate
+            inner_integration
+            0. cut_bl ~abs_err ~rel_err)
+          cut_bl
+      in
+      try
+        (if cut_bl =~ 0. 
+         then inner_integration (cut_bl /. 2.)
+         else outer_integration ())
+        |> log
+        |> (+.) base_ll
+      with
+        | Gsl_error.Gsl_exn (Gsl_error.ETOL, _) ->
+            incr n_exceptions;
+            perform (upper_limit /. 2.)
+      in perform upper_limit
