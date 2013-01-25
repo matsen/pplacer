579455c37bf928ee426cd68822f0e254b618b670
Connor McCoy <cmccoy@fhcrc.org>
2013-01-25 13:19:30 -0800

    Add `--unitize` flag to guppy squash

--- pplacer_src/guppy_squash.ml
+++ pplacer_src/guppy_squash.ml
@@ -110,6 +110,8 @@ object (self)
                      "Apply rounding with %d sig figs and cutoff %g to each
                      placerun before clustering"
                      round_sig_figs round_cutoff))
+  val unitize = flag "--unitize"
+    (Plain (false, "Unitize masses after bootstrap resampling"))
 
   method specl =
     super_mass#specl
@@ -122,7 +124,8 @@ object (self)
     @ [
       int_flag nboot;
       string_flag tax_cluster_mode;
-      toggle_flag round
+      toggle_flag round;
+      toggle_flag unitize;
     ]
 
   method desc =
@@ -159,7 +162,8 @@ object (self)
                    (List.map maybe_round prl)
     in
     let path = (^) (self#single_prefix ()) in
-    let nboot = fv nboot in
+    let nboot = fv nboot
+    and unitize = fv unitize in
     self#check_placerunl prl;
     if 0 = nboot then begin
       (* bootstrap turned off *)
@@ -189,9 +193,13 @@ object (self)
     else begin
       let pad_width = find_zero_pad_width nboot in
       let rng = self#rng in
+      let boot_placerun = match unitize with
+        | true -> Bootstrap.boot_placerun rng |- Placerun.unitize
+        | false -> Bootstrap.boot_placerun rng
+      in
       for i=1 to nboot do
         dprintf "running bootstrap %d of %d\n" i nboot;
-        let boot_prl = List.map (Bootstrap.boot_placerun rng) prl in
+        let boot_prl = List.map boot_placerun prl in
         let (_, cluster_t, _) = our_make_cluster refpkgo mode_str boot_prl in
         Newick_gtree.to_file
           cluster_t
