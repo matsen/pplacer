a712f7db79aa967ecaef4893f1d8a81f40c3e401
Erick Matsen <matsen@fhcrc.org>
2013-05-06 11:41:23 -0700

    consistent naming and comments

--- pplacer_src/som.ml
+++ pplacer_src/som.ml
@@ -1,8 +1,5 @@
 (*
-Code to perform Support Overlap Minimzation.
-- trans: the principal components
-- trans_part: the first three rows of trans
-- vars: variances
+Code to perform Support Overlap Minimzation as described in Matsen and Evans, 2013.
 *)
 
 open Ppatteries
@@ -57,38 +54,47 @@ let rot_mat angles  =
   m.{2,2} <-  cos_theta;
   m
 
-(*
-When we are doing PCA, we want to transform our samples from the usual basis in
-which they are expressed to the basis of k principal components. In the SOM
-case, we want to apply a rotation to the plane spanned by the principal
-component vectors. We can do this by multiplying the original kxp transormation
-on the left by a kxk rotation.
+(* When we are doing PCA, we want to transform our samples from the usual basis
+ * in which they are expressed to the basis of k principal components. In the
+ * SOM case, we want to apply a rotation to the plane spanned by the principal
+ * component vectors. If expressed in the usual way for transformations
+ * (vectors are columns of transformation matrix) then we would multiply on the
+ * right. However, in this case it's easier if the eigenvectors are the _rows_
+ * of the eigenvector matrix, so we multiply on the left by the transpose.
 *)
-let rotate_trans trans_part angles =
-  let result = Gsl_matrix.copy trans_part in
+let rotate_vects vects_part angles =
+  let result = Gsl_matrix.copy vects_part in
   let trans_mat_mult =
     Gsl_blas.gemm ~ta:Gsl_blas.Trans ~tb:Gsl_blas.NoTrans ~alpha:1. ~beta:0.
   in
-  trans_mat_mult ~a:(rot_mat angles) ~b:trans_part ~c:result;
+  trans_mat_mult ~a:(rot_mat angles) ~b:vects_part ~c:result;
   result
 
-(* Once we know the appropriate angles, we can rotate the variances into place. *)
-let rotate_vars vars angles =
-  let vars_part = Gsl_vector.of_array (Array.sub vars 0 3) in
-  let vars_result = Gsl_vector.create ~init:0. 3 in
-  let vars_rest = Array.sub vars 3 ((Array.length vars) - 3) in
+(* We also need to "rotate" the variances, i.e. change them such that they
+ * actually correspond to the rotated eigenvectors. Recall that
+ * Var(aX + bY) = a^2 Var(X) + b^2 Var(Y) when X and Y are independent.
+ * Thus, when we are looking at a linear transformation of a vector of
+ * whose ith component is
+ * \sum_j a_{ij} X_j
+ * the variance is
+ * \sum_j a_{ij}^2 Var(X_j).
+ * As can be seen, we do this for a maximum of three dimensions. *)
+let rotate_vals vals angles =
+  let vals_part = Gsl_vector.of_array (Array.sub vals 0 3) in
+  let vals_result = Gsl_vector.create ~init:0. 3 in
+  let vals_rest = Array.sub vals 3 ((Array.length vals) - 3) in
   let rot = rot_mat angles in
   (* Square the elements of the rotation matrix. *)
   Gsl_matrix.mul_elements rot rot;
-  (* Multiply its transpose by our vars to get the rotated vars. *)
-  Gsl_blas.gemv Gsl_blas.Trans ~alpha:1. ~beta:0. ~a:rot ~x:vars_part ~y:vars_result;
-  Array.append (Gsl_vector.to_array vars_result) vars_rest
+  (* Multiply its transpose by our vals to get the rotated vals. *)
+  Gsl_blas.gemv Gsl_blas.Trans ~alpha:1. ~beta:0. ~a:rot ~x:vals_part ~y:vals_result;
+  Array.append (Gsl_vector.to_array vals_result) vals_rest
 
 (* Measures the overlap between the tranform vector components when rotated
  * through the given angles. *)
-let overlap trans_part dims angles =
-  let rotated_trans = rotate_trans trans_part angles in
-  let row i = Gsl_matrix.row rotated_trans i
+let overlap vects_part dims angles =
+  let rotated_vects = rotate_vects vects_part angles in
+  let row i = Gsl_matrix.row rotated_vects i
   and indices = match dims with
   | 2 -> [(0, 1)]
   | 3 -> [(0, 1); (0, 2); (1, 2)]
@@ -105,11 +111,11 @@ let overlap trans_part dims angles =
   overlapper indices
 
 (* Performs overlap minimization using Brent. *)
-let min_overlap trans_part dims =
-  let tolerance = (overlap trans_part dims [|0.; 0.; 0.|]) *. (0.0001) in
+let min_overlap vects_part dims =
+  let tolerance = (overlap vects_part dims [|0.; 0.; 0.|]) *. (0.0001) in
   match dims with
   | 2 ->
-      let obj_fun phi = overlap trans_part dims [|phi; 0.; 0.|] in
+      let obj_fun phi = overlap vects_part dims [|phi; 0.; 0.|] in
       let min = Minimization.brent
         ~start_finder:Minimization.robust_start_finder
         obj_fun
@@ -119,7 +125,7 @@ let min_overlap trans_part dims =
       in
       [|min; 0.; 0.|]
   | 3 ->
-      let obj_fun = overlap trans_part dims
+      let obj_fun = overlap vects_part dims
       and start = [|0.; 0.; 0.|]
       and lower = Array.make 3 (-. Gsl_math.pi)
       and upper = Array.make 3 (Gsl_math.pi)
@@ -143,46 +149,46 @@ let min_overlap trans_part dims =
   | _ -> failwith "Can only rotate in 2 or 3 dimensionss\n"
 
 (* In situations where the roation takes the variances out of order, this
- * reorders both the vars and the trans vectors. *)
-let reordered_by_vars vars trans dims =
-  let reordered_vars = Array.copy vars
-  and orig_var_i x = Array.findi (fun y -> y = x) vars
+ * reorders both the vals and the vects vectors. *)
+let reordered_by_vals vals vects dims =
+  let reordered_vals = Array.copy vals
+  and orig_var_i x = Array.findi (fun y -> y = x) vals
   in
-  Array.sort (fun x y -> compare y x) reordered_vars;
-  let reordered_trans = Array.map
-    (fun var -> trans.(orig_var_i var))
-    (Array.sub reordered_vars 0 dims)
+  Array.sort (fun x y -> compare y x) reordered_vals;
+  let reordered_vects = Array.map
+    (fun var -> vects.(orig_var_i var))
+    (Array.sub reordered_vals 0 dims)
   in
-  (reordered_vars, reordered_trans)
+  (reordered_vals, reordered_vects)
 
 (* A vector v has the same variance as -v; we would like whichever most
- * closely matches the original trans. This makes it easier to compare the
- * original to the SOM trans. *)
-let flip_axes ~orig_trans ~new_trans =
+ * closely matches the original vects. This makes it easier to compare the
+ * original to the SOM vects. *)
+let flip_axes ~orig_vects ~new_vects =
   let flip vec = Array.map (fun x -> -1. *. x) vec in
   let flipper i vec =
     let flipped = flip vec in
-    if Pca.dot vec orig_trans.(i) > Pca.dot flipped orig_trans.(i) then
+    if Pca.dot vec orig_vects.(i) > Pca.dot flipped orig_vects.(i) then
       vec
     else
       flipped
   in
-  Array.mapi flipper new_trans
+  Array.mapi flipper new_vects
 
-(* Returns a tuple of the roated trans (as an array of arrays), the rotated
- * vars. *)
-let som_rotation trans dims vars =
-  let trans_part = Gsl_matrix.of_arrays (Array.sub trans 0 3) in
+(* Returns a tuple of the roated vects (as an array of arrays), the rotated
+ * vals. *)
+let som_rotation vects dims vals =
+  let vects_part = Gsl_matrix.of_arrays (Array.sub vects 0 3) in
   (* Where all the real work is - find min(s) *)
-  let min = min_overlap trans_part dims in
-  let vars = rotate_vars vars min
-  and trans_part = Gsl_matrix.to_arrays (rotate_trans trans_part min)
+  let min = min_overlap vects_part dims in
+  let vals = rotate_vals vals min
+  and vects_part = Gsl_matrix.to_arrays (rotate_vects vects_part min)
   in
-  let vars, trans_part = reordered_by_vars vars trans_part dims in
-  let flipped_trans = flip_axes ~orig_trans:trans ~new_trans:trans_part in
-  (* We want our final trans to be the same as the original but with the first
+  let vals, vects_part = reordered_by_vals vals vects_part dims in
+  let flipped_vects = flip_axes ~orig_vects:vects ~new_vects:vects_part in
+  (* We want our final vects to be the same as the original but with the first
    * dims dimensions rotated. *)
-  let final_trans = Array.copy trans in
-  Array.blit flipped_trans 0 final_trans 0 dims;
-  (vars, final_trans)
+  let final_vects = Array.copy vects in
+  Array.blit flipped_vects 0 final_vects 0 dims;
+  (vals, final_vects)
 
