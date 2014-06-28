18429d963c660361827612ca303a67f5466215ad
Erick Matsen <matsen@fhcrc.org>
2014-06-27 17:33:57 -0700

    Removing out of date AUTHORS and date++.

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'pplacer'
-copyright = u'2011-2013, Erick Matsen and Aaron Gallagher'
+copyright = u'2011-2014, Matsen Group'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
