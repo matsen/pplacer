cbd6045c2f0a95b664689dbf91edc4247b498aa4
David Rich <david.rich27@gmail.com>
2025-07-08 15:10:02 -0600

    fix version, added INSTALL.md

--- version.ml
+++ version.ml
@@ -1,1 +1,7 @@
-let version = "%%VERSION%%"
+let version = 
+  try
+    let ic = Unix.open_process_in "git describe --tags --always --dirty 2>/dev/null" in
+    let version = input_line ic in
+    close_in ic;
+    version
+  with _ -> "dev"
