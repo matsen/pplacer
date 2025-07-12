7e141caccf35b539270c982a9ed9ed487969ea7f
Erick Matsen <ematsen@gmail.com>
2025-07-12 08:33:29 -0700

    fix from @nojb

--- version.ml
+++ version.ml
@@ -2,6 +2,6 @@ let version =
   try
     let ic = Unix.open_process_in "git describe --tags --always --dirty 2>/dev/null" in
     let version = input_line ic in
-    close_in ic;
+    ignore (Unix.close_process_in ic);
     version
   with _ -> "dev"
